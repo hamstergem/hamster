8251f5d2717d03c55ac449cd74b829f5e1fc0901
Simon Harris <haruki.zaemon@gmail.com>
2009-10-21 16:32:00 +1100

    First cut proof-of-concept done.

--- lib/hamster/trie.rb
+++ lib/hamster/trie.rb
@@ -1,6 +1,56 @@
 module Hamster
   
   class Trie
+    
+    class Entry
+
+      attr_reader :value
+
+      def initialize(key, value)
+        @key = key
+        @value = value
+      end
+
+      def for?(key)
+        @key == key
+      end
+
+    end
+    
+    def initialize(significant_bits)
+      @significant_bits = significant_bits
+      @entries = []
+      @children = []
+    end
+
+    def put(key, value)
+      index = index_for(key)
+      entry = @entries[index]
+      if entry && !entry.for?(key)
+        child = @children[index] ||= Trie.new(@significant_bits + 5)
+        child.put(key, value)
+      else
+        @entries[index] = Entry.new(key, value)
+      end
+    end
+
+    def get(key)
+      index = index_for(key)
+      entry = @entries[index]
+      if entry
+        if entry.for?(key)
+          entry.value
+        else
+          child = @children[index]
+          child.get(key) if child
+        end
+      end
+    end
+    
+    def index_for(key)
+      (key.hash.abs >> @significant_bits) & 31
+    end
+
   end
   
 end
--- spec/hamster/trie_spec.rb
+++ spec/hamster/trie_spec.rb
@@ -1,5 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Hamster::Trie do
+  
+  before do
+    @trie = Hamster::Trie.new(0)
+
+    ("A".."Z").each do |letter|
+      @trie.put(letter, letter.downcase)
+    end    
+  end
+
+  describe "#get" do
+
+    it "returns values associated with keys that exist" do
+      ("A".."Z").each do |letter|
+        @trie.get(letter).should == letter.downcase
+      end
+    end
+    
+    it "returns nil for keys that don't exist" do
+      @trie.get("missing").should be_nil
+    end
+    
+  end
 
 end
