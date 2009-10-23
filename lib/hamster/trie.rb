85ea9a8e09b7d82e92769ed0f69190b846dd8216
Simon Harris <haruki.zaemon@gmail.com>
2009-10-23 20:22:06 +1100

    Idiomatic clone

--- lib/hamster/trie.rb
+++ lib/hamster/trie.rb
@@ -4,25 +4,29 @@ module Hamster
 
     include Enumerable
 
-    def initialize(significant_bits = 0, entries = [], children = [])
+    def initialize(significant_bits = 0)
       @significant_bits = significant_bits
-      @entries = entries
-      @children = children
+      @entries = []
+      @children = []
     end
 
+    # Returns the number of key-value pairs in the trie.
     def size
       # TODO: This definitely won't scale!
       to_a.size
     end
 
+    # Returns <tt>true</tt> if the trie contains no key-value pairs.
     def empty?
       size == 0
     end
 
+    # Returns <tt>true</tt> if the given key is present in the trie.
     def has_key?(key)
       !! get(key)
     end
 
+    # Calls <tt>block</tt> once for each key in the trie, passing the key-value pair as parameters.
     def each
       block_given? or return enum_for(__method__)
       @entries.each { |entry| yield entry.key, entry.value if entry }
@@ -32,9 +36,26 @@ module Hamster
       self
     end
 
+    # Associates the value given by with the key.
     def put(key, value)
       self.dup.put!(key, value)
     end
+
+    # Retrieves the value corresponding to the given key. If not found, returns nil.
+    def get(key)
+      index = index_for(key)
+      entry = @entries[index]
+      if entry
+        if entry.has_key?(key)
+          entry.value
+        else
+          child = @children[index]
+          child.get(key) if child
+        end
+      end
+    end
+
+    protected
 
     def put!(key, value)
       index = index_for(key)
@@ -51,25 +72,14 @@ module Hamster
       end
       self
     end
-
-    def get(key)
-      index = index_for(key)
-      entry = @entries[index]
-      if entry
-        if entry.has_key?(key)
-          entry.value
-        else
-          child = @children[index]
-          child.get(key) if child
-        end
-      end
-    end
-
-    def dup
-      self.class.new(@significant_bits, @entries.dup, @children.dup)
-    end
-
+key
     private
+
+    def initialize_copy(other)
+      @significant_bits = other.instance_eval{@significant_bits}
+      @entries = other.instance_eval{@entries}.dup
+      @children = other.instance_eval{@children}.dup
+    end
 
     def index_for(key)
       (key.hash.abs >> @significant_bits) & 31
