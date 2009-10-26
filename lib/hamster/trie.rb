e273dd8417f7f19e195d76f861d395321dc1dd12
Simon Harris <haruki.zaemon@gmail.com>
2009-10-26 14:33:40 +1100

    Make Entry a part of Trie

--- lib/hamster/trie.rb
+++ lib/hamster/trie.rb
@@ -111,6 +111,21 @@ module Hamster
       (key.hash.abs >> @significant_bits) & 31
     end
 
+    class Entry
+
+      attr_reader :key, :value
+
+      def initialize(key, value)
+        @key = key
+        @value = value
+      end
+
+      def has_key?(key)
+        @key.eql?(key)
+      end
+
+    end
+
   end
 
 end
