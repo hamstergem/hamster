9ba6694cf4fb1ba6a40809a2a484b4254ccc3943
Simon Harris <haruki.zaemon@gmail.com>
2009-11-29 21:15:59 +1100

    More cleanup and specs.

--- spec/hamster/hash/get_spec.rb
+++ spec/hamster/hash/get_spec.rb
@@ -5,16 +5,23 @@ describe Hamster::Hash do
   describe "#get" do
 
     before do
-      @hash = Hamster::Hash.new
-      @hash = @hash.put("A", "aye")
+      @hash = Hamster::Hash["A" => "aye", "B" => "bee", "C" => "see"]
     end
 
-    it "returns the value for an existing key" do
-      @hash.get("A").should == "aye"
+    [
+      ["A", "aye"],
+      ["B", "bee"],
+      ["C", "see"],
+    ].each do |key, value|
+
+      it "returns the value ('#{value}') for an existing key ('#{key}')" do
+        @hash.get(key).should == value
+      end
+
     end
 
-    it "returns nil for a non-existing key" do
-      @hash.get("B").should be_nil
+    it "returns nil for a non-existing key ('D')" do
+      @hash.get("D").should be_nil
     end
 
   end
