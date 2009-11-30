8d978ac8bf7322362eea91fdca129f4f50fb8659
Simon Harris <haruki.zaemon@gmail.com>
2009-11-30 15:00:08 +1100

    Cleanup.

--- spec/hamster/hash/eql_spec.rb
+++ spec/hamster/hash/eql_spec.rb
@@ -2,22 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Hamster::Hash do
 
-  describe "#eql?" do
+  [:==, :eql?].each do |method|
 
-    [
-      [[], [], true],
-      [["A" => "aye"], [], false],
-      [[], ["A" => "aye"], false],
-      [["A" => "aye"], ["A" => "aye"], true],
-      [["A" => "aye"], ["B" => "bee"], false],
-      [["A" => "aye", "B" => "bee"], ["A" => "aye"], false],
-      [["A" => "aye"], ["A" => "aye", "B" => "bee"], false],
-      [["A" => "aye", "B" => "bee", "C" => "see"], ["A" => "aye", "B" => "bee", "C" => "see"], true],
-      [["C" => "see", "A" => "aye", "B" => "bee"], ["A" => "aye", "B" => "bee", "C" => "see"], true],
-    ].each do |a, b, result|
+    describe "##{method}" do
 
-      it "returns #{result} for #{a.inspect} and #{b.inspect}" do
-        Hamster::Hash[*a].eql?(Hamster::Hash[*b]).should == result
+      [
+        [[], [], true],
+        [["A" => "aye"], [], false],
+        [[], ["A" => "aye"], false],
+        [["A" => "aye"], ["A" => "aye"], true],
+        [["A" => "aye"], ["B" => "bee"], false],
+        [["A" => "aye", "B" => "bee"], ["A" => "aye"], false],
+        [["A" => "aye"], ["A" => "aye", "B" => "bee"], false],
+        [["A" => "aye", "B" => "bee", "C" => "see"], ["A" => "aye", "B" => "bee", "C" => "see"], true],
+        [["C" => "see", "A" => "aye", "B" => "bee"], ["A" => "aye", "B" => "bee", "C" => "see"], true],
+      ].each do |a, b, result|
+
+        it "returns #{result} for #{a.inspect} and #{b.inspect}" do
+          Hamster::Hash[*a].send(method, Hamster::Hash[*b]).should == result
+        end
+
       end
 
     end
--- spec/hamster/set/eql_spec.rb
+++ spec/hamster/set/eql_spec.rb
@@ -2,22 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Hamster::Set do
 
-  describe "#eql?" do
+  [:==, :eql?].each do |method|
 
-    [
-      [[], [], true],
-      [["A"], [], false],
-      [[], ["A"], false],
-      [["A"], ["A"], true],
-      [["A"], ["B"], false],
-      [["A", "B"], ["A"], false],
-      [["A"], ["A", "B"], false],
-      [["A", "B", "C"], ["A", "B", "C"], true],
-      [["C", "A", "B"], ["A", "B", "C"], true],
-    ].each do |a, b, result|
+    describe "##{method}" do
 
-      it "returns #{result} for #{a.inspect} and #{b.inspect}" do
-        Hamster::Set[*a].eql?(Hamster::Set[*b]).should == result
+      [
+        [[], [], true],
+        [["A"], [], false],
+        [[], ["A"], false],
+        [["A"], ["A"], true],
+        [["A"], ["B"], false],
+        [["A", "B"], ["A"], false],
+        [["A"], ["A", "B"], false],
+        [["A", "B", "C"], ["A", "B", "C"], true],
+        [["C", "A", "B"], ["A", "B", "C"], true],
+      ].each do |a, b, result|
+
+        it "returns #{result} for #{a.inspect} and #{b.inspect}" do
+          Hamster::Set[*a].send(method, Hamster::Set[*b]).should == result
+        end
+
       end
 
     end
