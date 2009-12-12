0668078de0d320bc479441730daafd44fafbc953
Simon Harris <haruki.zaemon@gmail.com>
2009-12-12 22:02:40 +1100

    Actually, #fold *is* the same as reduce!
    This reverts commit 1fd4d8c3c76a0fa7c636dbddc41f270adb05d4f3.

--- spec/hamster/list/reduce_spec.rb
+++ spec/hamster/list/reduce_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Hamster::List do
 
-  [:reduce, :inject].each do |method|
+  [:reduce, :inject, :fold].each do |method|
 
     describe "##{method}" do
 
