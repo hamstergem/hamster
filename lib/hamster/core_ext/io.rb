268f3e36705424bd2ea66c1b1bd4f594a19294a3
Simon Harris <haruki.zaemon@gmail.com>
2009-12-15 23:40:14 +1100

    Added IO#to_list.

---
+++ lib/hamster/core_ext/io.rb
@@ -0,0 +1,29 @@
+require 'hamster/list'
+
+module Hamster
+
+  module CoreExt
+
+    module IO
+
+      def to_list(sep = $/)
+        line = gets(sep)
+        if line
+          Stream.new(line) { to_list }
+        else
+          EmptyList
+        end
+
+      end
+
+    end
+
+  end
+
+end
+
+class IO
+
+  include Hamster::CoreExt::IO
+
+end
---
+++ spec/hamster/core_ext/io_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+require 'hamster/core_ext/io'
+
+describe IO do
+
+  describe "#to_list" do
+
+    it "returns an equivalent list" do
+      File.open(File.dirname(__FILE__) + "/io_spec.txt") do |io|
+        io.to_list.should == Hamster.list("A\n", "B\n", "C\n")
+      end
+    end
+
+  end
+
+end
