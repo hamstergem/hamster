c925d2ce47fcc8fba747d2961f5da371490bfb3b
Simon Harris <haruki.zaemon@gmail.com>
2010-01-08 15:21:33 +1100

    Implement List#join.

--- lib/hamster/list.rb
+++ lib/hamster/list.rb
@@ -235,6 +235,10 @@ module Hamster
       Hamster.list(*to_a.sort_by(&block))
     end
 
+    def join(sep = nil)
+      to_a.join(sep)
+    end
+
     def eql?(other)
       return false unless other.is_a?(List)
 
