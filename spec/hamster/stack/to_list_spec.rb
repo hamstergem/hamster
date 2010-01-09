4cdbab44318407eb40ca0afc8848cf3febd53983
Simon Harris <haruki.zaemon@gmail.com>
2010-01-09 22:26:16 +1100

    Implement Stack#to_list.

---
+++ spec/hamster/stack/to_list_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+require 'hamster/stack'
+require 'hamster/list'
+
+describe Hamster::Stack do
+
+  describe "#to_list" do
+
+    [
+      [[], []],
+      [["A"], ["A"]],
+      [["A", "B", "C"], ["C", "B", "A"]],
+    ].each do |values, expected|
+
+      describe "on #{values.inspect}" do
+
+        before do
+          @stack = Hamster.stack(*values)
+          @result = @stack.to_list
+        end
+
+        it "returns #{expected.inspect}" do
+          @result.should == Hamster.list(*expected)
+        end
+
+      end
+
+    end
+
+  end
+
+end
