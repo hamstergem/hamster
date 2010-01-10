299f725e3fb8b097f95d59549385ee8cc8d01480
Simon Harris <haruki.zaemon@gmail.com>
2010-01-10 21:58:57 +1100

    Implement Set#maximum (aliased as #max).

---
+++ spec/hamster/set/maximum_spec.rb
@@ -0,0 +1,69 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+require 'hamster/set'
+
+describe Hamster::Set do
+
+  [:maximum, :max].each do |method|
+
+    describe "##{method}" do
+
+      describe "with a block" do
+
+        [
+          [[], nil],
+          [["A"], "A"],
+          [["Ichi", "Ni", "San"], "Ichi"],
+        ].each do |values, expected|
+
+          describe "on #{values.inspect}" do
+
+            before do
+              original = Hamster.set(*values)
+              pending do
+                @result = original.send(method) { |maximum, item| item.length <=> maximum.length }
+              end
+            end
+
+            it "returns #{expected.inspect}" do
+              @result.should == expected
+            end
+
+          end
+
+        end
+
+      end
+
+      describe "without a block" do
+
+        [
+          [[], nil],
+          [["A"], "A"],
+          [["Ichi", "Ni", "San"], "San"],
+        ].each do |values, expected|
+
+          describe "on #{values.inspect}" do
+
+            before do
+              original = Hamster.set(*values)
+              pending do
+                @result = original.send(method)
+              end
+            end
+
+            it "returns #{expected.inspect}" do
+              @result.should == expected
+            end
+
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
