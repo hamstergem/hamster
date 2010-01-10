a98ac87c1c8854c6e209296127ebbf550b606cf3
Simon Harris <haruki.zaemon@gmail.com>
2010-01-10 13:57:36 +1100

    Implement List#intersperse.

--- lib/hamster/list.rb
+++ lib/hamster/list.rb
@@ -248,6 +248,11 @@ module Hamster
       tail.reduce(head.to_s) { |string, item| string << sep << item.to_s }
     end
 
+    def intersperse(sep)
+      return self if tail.empty?
+      Stream.new(head) { Stream.new(sep) { tail.intersperse(sep) } }
+    end
+
     def eql?(other)
       return false unless other.is_a?(List)
 
