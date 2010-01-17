f7353d0c11a846ad0a183401d83f053dbf3fd2b8
Simon Harris <haruki.zaemon@gmail.com>
2010-01-17 14:12:04 +1100

    Implement Hash#inspect.

---
+++ spec/hamster/hash/inspect_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+require 'hamster/hash'
+
+describe Hamster::Hash do
+
+  describe "#inspect" do
+
+    [
+      [[], "{}"],
+      [["A" => "aye"], "{\"A\" => \"aye\"}"],
+      [[1 => :one, 2 => :two, 3 => :three], "{1 => :one, 2 => :two, 3 => :three}"]
+    ].each do |values, expected|
+
+      describe "on #{values.inspect}" do
+
+        before do
+          original = Hamster.hash(*values)
+          @result = original.inspect
+        end
+
+        it "returns #{expected.inspect}" do
+          @result.should == expected
+        end
+
+      end
+
+    end
+
+  end
+
+end
