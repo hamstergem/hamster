2d8569de85c1801c154b62d7bee907aa229900a3
Simon Harris <haruki.zaemon@gmail.com>
2010-01-21 19:45:35 +1100

    Determine maximum stack depth at runtime.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 # Common spec-related code goes here
 
-STACK_OVERFLOW_DEPTH = 10000
+STACK_OVERFLOW_DEPTH = begin
+  def calculate_stack_overflow_depth(n)
+    calculate_stack_overflow_depth(n + 1)
+  rescue SystemStackError
+    n
+  end
+  calculate_stack_overflow_depth(2)
+end
