fcfcb8b1c1666195963153a184c09ee2f2848408
Simon Harris <haruki.zaemon@gmail.com>
2010-01-27 09:49:20 +1100

    Ensure Hash uses #eql.

--- spec/hamster/hash/has_key_spec.rb
+++ spec/hamster/hash/has_key_spec.rb
@@ -9,10 +9,10 @@ describe Hamster::Hash do
     describe "#has_key?" do
 
       before do
-        @hash = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", nil => "NIL")
+        @hash = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", nil => "NIL", 2.0 => "two")
       end
 
-      ["A", "B", "C", nil].each do |key|
+      ["A", "B", "C", nil, 2.0].each do |key|
 
         it "returns true for an existing key (#{key.inspect})" do
           @hash.send(method, key).should == true
@@ -22,6 +22,10 @@ describe Hamster::Hash do
 
       it "returns false for a non-existing key" do
         @hash.send(method, "D").should == false
+      end
+
+      it "uses #eql? for equality" do
+        @hash.send(method, 2).should == false
       end
 
     end
