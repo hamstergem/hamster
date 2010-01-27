8c4e465bc3cd9dffa7b06682290a9c602a0f88e9
Simon Harris <haruki.zaemon@gmail.com>
2010-01-27 14:03:03 +1100

    Simplify require statements.

--- spec/hamster/set/eql_spec.rb
+++ spec/hamster/set/eql_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path('../../spec_helper', File.dirname(__FILE__))
 
 require 'set'
 require 'hamster/set'
