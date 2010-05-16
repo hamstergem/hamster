0f97079523ec8abcbd919a19f69f97e767e6a4dc
Simon Harris <haruki.zaemon@gmail.com>
2010-05-16 21:45:54 +1000

    Implement Queue#to_a (aliased as #entries).

---
+++ spec/hamster/queue/to_a_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+require 'hamster/queue'
+
+describe Hamster::Queue do
+
+  [:to_a, :entries].each do |method|
+
+    describe "##{method}" do
+
+      [
+        [],
+        ["A"],
+        ["A", "B", "C"],
+      ].each do |values|
+
+        describe "on #{values.inspect}" do
+
+          before do
+            @queue = Hamster.queue(*values)
+            @result = @queue.send(method)
+          end
+
+          it "returns #{values.inspect}" do
+            @result.should == values
+          end
+
+          it "returns a mutable array" do
+            @result.last.should_not == "The End"
+            @result << "The End"
+            @result.last.should == "The End"
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
