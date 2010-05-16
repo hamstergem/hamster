10ba2d530768e3dcc1c7c9930324f1bf85af30ba
Simon Harris <haruki.zaemon@gmail.com>
2010-05-16 21:47:07 +1000

    Implement Queue#to_ary.

---
+++ spec/hamster/queue/to_ary_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+require 'hamster/queue'
+
+describe Hamster::Queue do
+
+  describe "#to_ary" do
+
+    describe "enables implicit conversion to" do
+
+      before do
+        @queue = Hamster.queue("A", "B", "C", "D")
+      end
+
+      it "block parameters" do
+        def func(&block)
+          yield(@queue)
+        end
+        func do |a, b, *c|
+          a.should == "A"
+          b.should == "B"
+          c.should == ["C", "D"]
+        end
+      end
+
+      it "method arguments" do
+        def func(a, b, *c)
+          a.should == "A"
+          b.should == "B"
+          c.should == ["C", "D"]
+        end
+        func(*@queue)
+      end
+
+      it "works with splat" do
+        array = *@queue
+        array.should == ["A", "B", "C", "D"]
+      end
+
+    end
+
+  end
+
+end
