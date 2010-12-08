7f5bce2e50b5b7356c9e55c7164536de5e6ab24d
Simon Harris <haruki_zaemon@mac.com>
2010-12-08 17:43:46 +1100

    Spec Vector#to_ary.

---
+++ spec/hamster/vector/to_ary_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+require 'hamster/vector'
+
+describe Hamster::Vector do
+
+  describe "#to_ary" do
+
+    describe "enables implicit conversion to" do
+
+      before do
+        @vector = Hamster.vector("A", "B", "C", "D")
+      end
+
+      it "block parameters" do
+        def func(&block)
+          yield(@vector)
+        end
+        func do |a, b, *c|
+          a.should == "A"
+          b.should == "B"
+          c.should == ["C", "D"]
+        end
+      end
+
+      it "method arguments" do
+        def func(a, b, *c)
+          a.should == "A"
+          b.should == "B"
+          c.should == ["C", "D"]
+        end
+        func(*@vector)
+      end
+
+      it "works with splat" do
+        array = *@vector
+        array.should == ["A", "B", "C", "D"]
+      end
+
+    end
+
+  end
+
+end
