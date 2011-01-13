2001423d09b4763a449f0e0df3c8b2820c88df3d
Simon Harris <haruki_zaemon@mac.com>
2011-01-13 16:45:31 +1100

    Explicitly check for RBX when calculating stack depth.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # Common spec-related code goes here
 
-STACK_OVERFLOW_DEPTH = if RUBY_VERSION =~ /^(ruby |\d+\.\d+\.\d+$)/
+STACK_OVERFLOW_DEPTH = if RUBY_ENGINE != "rbx"
   def calculate_stack_overflow_depth(n)
     calculate_stack_overflow_depth(n + 1)
   rescue SystemStackError
