6b1980a00a631daa9cdbac51de7dff27077de659
Simon Harris <haruki_zaemon@mac.com>
2011-01-13 17:12:57 +1100

    Only perform the *actual* stack depth calculation on MRI; other platforms don't like it so much :)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # Common spec-related code goes here
 
-STACK_OVERFLOW_DEPTH = if RUBY_ENGINE != "rbx"
+STACK_OVERFLOW_DEPTH = if RUBY_ENGINE == "ruby"
   def calculate_stack_overflow_depth(n)
     calculate_stack_overflow_depth(n + 1)
   rescue SystemStackError
