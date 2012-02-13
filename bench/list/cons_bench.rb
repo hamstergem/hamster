ac054ba3caf47f3c33a116a2c1917c3d4995ffc1
Simon Harris <haruki_zaemon@mac.com>
2012-02-13 21:07:05 +1100

    List#append is for concatenating lists; List#cons is for well, consing :)

--- bench/list/cons_bench.rb
+++ bench/list/cons_bench.rb
@@ -6,33 +6,33 @@ require 'hamster/list'
 Benchmark.ips do |b|
   sml_list = Hamster.list(1)
   med_list = Hamster.list
-  100.times {|i| med_list = med_list.append(i)}
+  100.times {|i| med_list = med_list.cons(i)}
   lrg_list = Hamster.list
-  10000.times {|i| lrg_list = lrg_list.append(i)}
-  
-  b.report "append small" do |n|
+  10000.times {|i| lrg_list = lrg_list.cons(i)}
+
+  b.report "cons small" do |n|
     a = 0
     sml = sml_list
     while a < n
-      sml = sml.append(a)
+      sml = sml.cons(a)
       a += 1
     end
   end
-  
-  b.report "append medium" do |n|
+
+  b.report "cons medium" do |n|
     a = 0
     med = med_list
     while a < n
-      med = med.append(a)
+      med = med.cons(a)
       a += 1
     end
   end
-  
-  b.report "append large" do |n|
+
+  b.report "cons large" do |n|
     a = 0
     lrg = lrg_list
     while a < n
-      lrg = lrg.append(a)
+      lrg = lrg.cons(a)
       a += 1
     end
   end
