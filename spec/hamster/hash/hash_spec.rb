dc8e98c435400f7a6eb7d450ea4d00253698b81d
Trent Ogren <tedwardo2@gmail.com>
2012-08-17 08:34:23 -0500

    Use `Hash.new` only from subclasses in the specs

--- spec/hamster/hash/hash_spec.rb
+++ spec/hamster/hash/hash_spec.rb
@@ -4,28 +4,28 @@ require 'hamster/hash'
 
 describe Hamster::Hash do
 
-  describe "#new" do
+  describe "#hash" do
 
     it "values are sufficiently distributed" do
       (1..4000).
         each_slice(4).
-        map { |ka, va, kb, vb| Hamster::Hash.new(ka => va, kb => vb).hash }.
+        map { |ka, va, kb, vb| Hamster.hash(ka => va, kb => vb).hash }.
         uniq.
         size.should == 1000
     end
 
     it "differs given the same keys and different values" do
-      Hamster::Hash.new("ka" => "va").hash.should_not == Hamster::Hash.new("ka" => "vb").hash
+      Hamster.hash("ka" => "va").hash.should_not == Hamster.hash("ka" => "vb").hash
     end
 
     it "differs given the same values and different keys" do
-      Hamster::Hash.new("ka" => "va").hash.should_not == Hamster::Hash.new("kb" => "va").hash
+      Hamster.hash("ka" => "va").hash.should_not == Hamster.hash("kb" => "va").hash
     end
 
     describe "on an empty hash" do
 
       before do
-        @result = Hamster::Hash.new.hash
+        @result = Hamster.hash.hash
       end
 
       it "returns 0" do
@@ -49,12 +49,6 @@ describe Hamster::Hash do
       end
     end
 
-  end
-
-  describe "Hamster.hash" do
-    it "is an alias for Hash.new" do
-      Hamster.hash(:a => 1, :b => 2).should == Hamster::Hash.new(:a => 1, :b => 2)
-    end
   end
 
 end
