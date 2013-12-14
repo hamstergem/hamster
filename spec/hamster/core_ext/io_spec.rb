63ae55cca3347210f99fe787323d6583e93a7205
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-12-13 21:26:24 -0800

    We want to test each type of Io

--- spec/hamster/core_ext/io_spec.rb
+++ spec/hamster/core_ext/io_spec.rb
@@ -9,12 +9,16 @@ describe IO do
     end
 
     context "with a File" do
+      let(:io) { File.new(fixture_path("io_spec.txt")) }
+
       it "returns an equivalent list" do
         fixture("io_spec.txt").to_list.should == Hamster.list("A\n", "B\n", "C\n")
       end
     end
 
     context "with a StringIO" do
+      let(:io) { StringIO.new(fixture("io_spec.txt")) }
+
       it "returns an equivalent list" do
         StringIO.new("A\nB\nC\n").to_list.should == Hamster.list("A\n", "B\n", "C\n")
       end
