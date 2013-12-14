3474fe5c16f619151d5011192a9664589d749884
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-12-13 21:26:12 -0800

    We want to close the io after each test

--- spec/hamster/core_ext/io_spec.rb
+++ spec/hamster/core_ext/io_spec.rb
@@ -3,6 +3,11 @@ require "hamster/core_ext/io"
 
 describe IO do
   describe "#to_list" do
+
+    after(:each) do
+      io.close
+    end
+
     context "with a File" do
       it "returns an equivalent list" do
         fixture("io_spec.txt").to_list.should == Hamster.list("A\n", "B\n", "C\n")
