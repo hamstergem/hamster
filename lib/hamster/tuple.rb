08a8263bf75f1e72e6e43ede2a8b2b84e1cdf4d7
Alex Dowad <alexinbeijing@gmail.com>
2014-06-21 20:47:12 +0200

    More concise (and faster) implementation of Hamster::Tuple

--- lib/hamster/tuple.rb
+++ lib/hamster/tuple.rb
@@ -1,39 +1,20 @@
-require "forwardable"
 require "hamster/immutable"
 
 module Hamster
-  class Tuple
-    extend Forwardable
+  class Tuple < Array
     include Immutable
 
     def initialize(*items)
-      @items = items.freeze
-    end
-
-    def first
-      @items.first
-    end
-
-    def last
-      @items.last
+      super(items)
     end
 
     def eql?(other)
-      return true if other.equal?(self)
-      instance_of?(other.class) && @items.eql?(other.instance_variable_get(:@items))
+      other.is_a?(Tuple) && super
     end
-    def_delegator :self, :eql?, :==
-
-    def to_ary
-      @items
-    end
-
-    def to_a
-      @items.dup
-    end
+    alias :== :eql?
 
     def inspect
-      "(#{@items.inspect[1..-2]})"
+      "(#{super[1..-2]})"
     end
   end
 end
