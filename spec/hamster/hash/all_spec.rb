ff87c2fff1809d6c1c80ba02eeda53e65e3ee19d
Alex Dowad <alexinbeijing@gmail.com>
2014-07-01 21:18:25 +0200

    Working on Hash#all? spec

--- spec/hamster/hash/all_spec.rb
+++ spec/hamster/hash/all_spec.rb
@@ -4,74 +4,43 @@ require "hamster/hash"
 describe Hamster::Hash do
   let(:hash) { Hamster.hash(values) }
 
-  describe "#all?" do
-    let(:all?) { hash.all?(&block) }
+  [:all?, :forall?].each do |method|
+    describe "##{method}" do
+      context "when empty" do
+        let(:values) { Hash.new }
 
-    context "when empty" do
-      let(:values) { Hash.new }
+        context "without a block" do
+          it "returns true" do
+            hash.send(method).should == true
+          end
+        end
 
-      context "without a block"
-      context "with a block"
-    end
+        context "with a block" do
+          it "returns true" do
+            hash.send(method) { false }.should == true
+          end
+        end
+      end
 
-    context "when not empty" do
-      let(:values) { { "A" => 1, "B" => 2, "C" => 3 } }
+      context "when not empty" do
+        let(:values) { { "A" => 1, "B" => 2, "C" => 3 } }
 
-      context "without a block"
-      context "with a block"
+        context "without a block" do
+          it "returns true" do
+            hash.send(method).should == true
+          end
+        end
+
+        context "with a block" do
+          it "returns true if the block always returns true" do
+            hash.send(method) { true }.should == true
+          end
+
+          it "returns false if the block ever returns false" do
+            hash.send(method) { |k,v| k != 'C' }.should == false
+          end
+        end
+      end
     end
   end
 end
-#   [:all?, :forall?].each do |method|
-#
-#     describe "##{method}" do
-#
-#       describe "when empty" do
-#
-#         before do
-#           @hash = Hamster.hash
-#         end
-#
-#         it "with a block returns true" do
-#           @hash.send(method) {}.should == true
-#         end
-#
-#         it "with no block returns true" do
-#           @hash.send(method).should == true
-#         end
-#
-#       end
-#
-#       describe "when not empty" do
-#
-#         before do
-#           @hash = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
-#         end
-#
-#         describe "with a block" do
-#
-#           it "returns true if the block always returns true" do
-#             @hash.send(method) { |key, value| true }.should == true
-#           end
-#
-#           it "returns false if the block ever returns false" do
-#             @hash.send(method) { |key, value| key == "D" || value == "dee" }.should == false
-#           end
-#
-#         end
-#
-#         describe "with no block" do
-#
-#           it "returns true" do
-#             @hash.send(method).should == true
-#           end
-#
-#         end
-#
-#       end
-#
-#     end
-#
-#   end
-#
-# end
