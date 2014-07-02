989bc0ddb41a8faaeace559a44f44d63d2a3a3d6
Alex Dowad <alexinbeijing@gmail.com>
2014-07-02 19:20:15 +0200

    Added spec for Hash#take_while

--- spec/hamster/hash/take_spec.rb
+++ spec/hamster/hash/take_spec.rb
@@ -15,4 +15,24 @@ describe Hamster::Hash do
       @hash.take(4).sort.should == [['A', 'aye'], ['B', 'bee'], ['C', 'see']]
     end
   end
+
+  describe "#take_while" do
+    it "passes elements to the block until the block returns nil/false" do
+      passed = nil
+      @hash.take_while { |k,v| passed = k; false }
+      ['A', 'B', 'C'].should include(passed)
+    end
+
+    it "returns an array of all elements before the one which returned nil/false" do
+      count = 0
+      result = @hash.take_while { count += 1; count < 3 }
+      [['A', 'aye'], ['B', 'bee'], ['C', 'see']].combination(2).should include(result.sort)
+    end
+
+    it "passes all elements if the block never returns nil/false" do
+      passed = []
+      @hash.take_while { |pair| passed << pair; true }.should == @hash.to_a
+      passed.sort.should == [['A', 'aye'], ['B', 'bee'], ['C', 'see']]
+    end
+  end
 end
