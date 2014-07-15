a88067ab6f1f7fe6cfdd7578df9a1be94fbdfb49
Ryan Mulligan <ryan@ryantm.com>
2014-07-14 20:03:59 -0700

    add Hamster.tuple constructor to match other collections

---
+++ spec/hamster/tuple/construction_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+require "hamster/queue"
+
+describe Hamster do
+  describe ".tuple" do
+    describe "with no arguments" do
+      let(:tuple) { Hamster.tuple }
+
+      it "always returns the same instance" do
+        expect(tuple).to eq(Hamster.tuple)
+      end
+
+      it "returns an empty queue" do
+        expect(tuple).to be_empty
+      end
+    end
+
+    describe "with a number of items" do
+      let(:tuple) { Hamster.tuple("A", "B", "C") }
+
+      it "always returns the same instance" do
+        expect(tuple).to eq(Hamster.tuple("A", "B", "C"))
+      end
+
+      it "same as constructing a new Tuple directly" do
+        expect(tuple).to eq(Hamster::Tuple.new("A", "B", "C"))
+      end
+    end
+  end
+end
