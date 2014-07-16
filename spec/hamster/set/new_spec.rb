0d078b852844e1b1f2121ad463781c860f83b1ab
Alex Dowad <alexinbeijing@gmail.com>
2014-07-16 22:19:44 +0200

    Added spec for Hamster::Set.new

---
+++ spec/hamster/set/new_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require "hamster/set"
+
+describe Hamster::Set do
+  describe "new" do
+    describe "from a subclass" do
+      before do
+        @subclass = Class.new(Hamster::Set)
+        @instance = @subclass.new("some", "values")
+      end
+
+      it "should return an instance of the subclass" do
+        @instance.class.should be @subclass
+      end
+
+      it "should return a frozen instance" do
+        @instance.frozen?.should be true
+      end
+    end
+  end
+end
