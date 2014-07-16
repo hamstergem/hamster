8411d55cd0315d78f149a3287ca5c3c396b3b932
Ryan Mulligan <ryan@ryantm.com>
2014-07-15 19:17:12 -0700

    change single quotes back to double quotes

--- spec/hamster/tuple/construction_spec.rb
+++ spec/hamster/tuple/construction_spec.rb
@@ -1,29 +1,29 @@
-require 'spec_helper'
-require 'hamster/tuple'
+require "spec_helper"
+require "hamster/tuple"
 
 describe Hamster do
-  describe '.tuple' do
-    context 'with no arguments' do
+  describe ".tuple" do
+    context "with no arguments" do
       let(:tuple) { Hamster.tuple }
 
-      it 'always returns the same instance' do
+      it "always returns the same instance" do
         expect(tuple).to eq(Hamster.tuple)
       end
 
-      it 'returns an empty queue' do
+      it "returns an empty queue" do
         expect(tuple).to be_empty
       end
     end
 
-    context 'with a number of items' do
-      let(:tuple) { Hamster.tuple('A', 'B', 'C') }
+    context "with a number of items" do
+      let(:tuple) { Hamster.tuple("A", "B", "C") }
 
-      it 'always returns the same instance' do
-        expect(tuple).to eq(Hamster.tuple('A', 'B', 'C'))
+      it "always returns the same instance" do
+        expect(tuple).to eq(Hamster.tuple("A", "B", "C"))
       end
 
-      it 'same as constructing a new Tuple directly' do
-        expect(tuple).to eq(Hamster::Tuple.new('A', 'B', 'C'))
+      it "same as constructing a new Tuple directly" do
+        expect(tuple).to eq(Hamster::Tuple.new("A", "B", "C"))
       end
     end
   end
