f701f10e0424e1efc1b9e7c63d4a67204062a6ce
Alex Dowad <alexinbeijing@gmail.com>
2014-07-22 10:29:11 +0200

    Reintroduce Hamster::Enumerable, minus the bloat

---
+++ lib/hamster/enumerable.rb
@@ -0,0 +1,46 @@
+require "forwardable"
+require "hamster/tuple"
+
+module Hamster
+  module Enumerable
+    extend Forwardable
+    include ::Enumerable
+
+    def remove
+      return self unless block_given?
+      filter { |item| !yield(item) }
+    end
+
+    def compact
+      filter { |item| !item.nil? }
+    end
+
+    def grep(pattern, &block)
+      filter { |item| pattern === item }.map(&block)
+    end
+
+    def product
+      reduce(1, &:*)
+    end
+
+    def sum
+      reduce(0, &:+)
+    end
+
+    def_delegator :self, :each, :foreach
+    def_delegator :self, :all?, :forall?
+    def_delegator :self, :any?, :exist?
+    def_delegator :self, :any?, :exists?
+    def_delegator :self, :to_a, :to_ary
+    def_delegator :self, :filter, :find_all
+    def_delegator :self, :filter, :select # make it return a Hamster collection (and possibly make it lazy)
+    def_delegator :self, :include?, :contains?
+    def_delegator :self, :include?, :elem?
+    def_delegator :self, :max, :maximum
+    def_delegator :self, :min, :minimum
+    def_delegator :self, :remove, :reject # make it return a Hamster collection (and possibly make it lazy)
+    def_delegator :self, :remove, :delete_if
+    def_delegator :self, :reduce, :fold
+    def_delegator :self, :reduce, :foldr
+  end
+end
