hamstergem/hamster

commit 01cb7615fc2b0670828dcf2286bf0a589427a78d

Remove redundant Sorter class

lib/hamster/list.rb
@@ -308,12 +308,12 @@ module Hamster
     end
 
     def sort(&comparator)
-      Stream.new { Sorter.new(self).sort(&comparator) }
+      Stream.new { super(&comparator).to_enum.to_list }
     end
 
     def sort_by(&transformer)
       return sort unless block_given?
-      Stream.new { Sorter.new(self).sort_by(&transformer) }
+      Stream.new { super(&transformer).to_enum.to_list }
     end
 
     def join(sep = "")

lib/hamster/set.rb
@@ -88,12 +88,12 @@ module Hamster
     def_delegator :self, :head, :first
 
     def sort(&comparator)
-      Stream.new { Sorter.new(self).sort(&comparator) }
+      Stream.new { super(&comparator).to_enum.to_list }
     end
 
     def sort_by(&transformer)
       return sort unless block_given?
-      Stream.new { Sorter.new(self).sort_by(&transformer) }
+      Stream.new { super(&transformer).to_enum.to_list }
     end
 
     def join(sep = nil)

lib/hamster/sorter.rb
@@ -1,25 +1,0 @@
-require "forwardable"
-require "hamster/immutable"
-require "hamster/core_ext/enumerator"
-
-module Hamster
-  class Sorter
-    include Enumerable
-    extend Forwardable
-    include Immutable
-
-    def initialize(collection)
-      @collection = collection
-    end
-
-    def_delegator :@collection, :each
-
-    def sort(&comparator)
-      super.to_enum.to_list
-    end
-
-    def sort_by(&transformer)
-      super.to_enum.to_list
-    end
-  end
-end

spec/hamster/sorter/immutable_spec.rb
@@ -1,9 +1,0 @@
-require "spec_helper"
-require "hamster/immutable"
-require "hamster/sorter"
-
-describe Hamster::Sorter do
-  it "includes Immutable" do
-    Hamster::Sorter.should include(Hamster::Immutable)
-  end
-end

commit 34708cdcae0a508db5f3a5f6971cfda5ba09942e

Fixed bug caused by 01cb761

lib/hamster/list.rb
@@ -6,7 +6,6 @@ require "hamster/undefined"
 require "hamster/enumerable"
 require "hamster/groupable"
 require "hamster/tuple"
-require "hamster/sorter"
 require "hamster/hash"
 require "hamster/set"
 

lib/hamster/set.rb
@@ -3,7 +3,6 @@ require "hamster/immutable"
 require "hamster/undefined"
 require "hamster/enumerable"
 require "hamster/groupable"
-require "hamster/sorter"
 require "hamster/trie"
 require "hamster/list"
 