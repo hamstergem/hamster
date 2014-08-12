35304f211b500ac529670e150a5c66675032c99a
Alex Dowad <alexinbeijing@gmail.com>
2014-08-12 19:36:18 +0200

    Added more tests for SortedSet#delete

--- spec/hamster/sorted_set/delete_spec.rb
+++ spec/hamster/sorted_set/delete_spec.rb
@@ -7,17 +7,23 @@ describe Hamster::SortedSet do
   end
 
   describe "#delete" do
+    context "on an empty set" do
+      it "returns an empty set" do
+        Hamster::EmptySortedSet.delete(0).should be(Hamster::EmptySortedSet)
+      end
+    end
+
     context "with an existing value" do
       before do
         @result = @original.delete("B")
       end
 
       it "preserves the original" do
-        @original.should == Hamster.sorted_set("A", "B", "C")
+        @original.should eql(Hamster.sorted_set("A", "B", "C"))
       end
 
       it "returns a copy with the remaining of values" do
-        @result.should == Hamster.sorted_set("A", "C")
+        @result.should eql(Hamster.sorted_set("A", "C"))
       end
     end
 
@@ -27,7 +33,7 @@ describe Hamster::SortedSet do
       end
 
       it "preserves the original values" do
-        @original.should == Hamster.sorted_set("A", "B", "C")
+        @original.should eql(Hamster.sorted_set("A", "B", "C"))
       end
 
       it "returns self" do
@@ -42,6 +48,21 @@ describe Hamster::SortedSet do
 
       it "returns the canonical empty set" do
         @result.should be(Hamster::EmptySortedSet)
+      end
+    end
+
+    1.upto(10) do |n|
+      values = (1..n).to_a
+      values.combination(3) do |to_delete|
+        expected = to_delete.reduce(values.dup) { |ary,val| ary.delete(val); ary }
+        describe "on #{values.inspect}, when deleting #{to_delete.inspect}" do
+          it "returns #{expected.inspect}" do
+            set = Hamster::SortedSet.new(values)
+            result = to_delete.reduce(set) { |s,val| s.delete(val) }
+            result.should eql(Hamster::SortedSet.new(expected))
+            result.to_a.should eql(expected)
+          end
+        end
       end
     end
   end
