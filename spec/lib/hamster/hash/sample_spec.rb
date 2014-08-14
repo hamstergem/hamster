99c5d5b8b43ba0e1cf20fc2bb17e1f92fed39946
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:03:06 +0200

    Updated Hash#sample spec

--- spec/lib/hamster/hash/sample_spec.rb
+++ spec/lib/hamster/hash/sample_spec.rb
@@ -3,14 +3,12 @@ require "hamster/hash"
 
 describe Hamster::Hash do
   describe "#sample" do
-    before do
-      @hash = Hamster::Hash.new((:a..:z).zip(1..26))
-    end
+    let(:hash) { Hamster::Hash.new((:a..:z).zip(1..26)) }
 
     it "returns a randomly chosen item" do
-      chosen = 150.times.map { @hash.sample }.sort.uniq
-      chosen.each { |item| @hash.should include(item[0]) }
-      @hash.each { |item| chosen.should include(item) }
+      chosen = 150.times.map { hash.sample }.sort.uniq
+      chosen.each { |item| hash.should include(item[0]) }
+      hash.each { |item| chosen.should include(item) }
     end
   end
 end
