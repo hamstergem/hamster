25f467257bf0f843fd216557003cdb8260697f51
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:02:05 +0200

    Updated style of Hash#put spec, added a test

--- spec/lib/hamster/hash/put_spec.rb
+++ spec/lib/hamster/hash/put_spec.rb
@@ -3,64 +3,67 @@ require "hamster/hash"
 
 describe Hamster::Hash do
   describe "#put" do
-    before do
-      @original = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
-    end
+    let(:hash) { Hamster.hash("A" => "aye", "B" => "bee", "C" => "see") }
 
-    describe "with a block" do
+    context "with a block" do
       it "passes the value to the block" do
-        @original.put("A") { |value| value.should == "aye" }
+        hash.put("A") { |value| value.should == "aye" }
       end
 
       it "replaces the value with the result of the block" do
-        result = @original.put("A") { |value| "FLIBBLE" }
+        result = hash.put("A") { |value| "FLIBBLE" }
         result.get("A").should == "FLIBBLE"
       end
 
       it "supports to_proc methods" do
-        result = @original.put("A", &:upcase)
+        result = hash.put("A", &:upcase)
         result.get("A").should == "AYE"
       end
     end
 
-    describe "with a unique key" do
-      before do
-        @result = @original.put("D", "dee")
-      end
+    context "with a unique key" do
+      let(:result) { hash.put("D", "dee") }
 
       it "preserves the original" do
-        @original.should == Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
+        result
+        hash.should eql(Hamster.hash("A" => "aye", "B" => "bee", "C" => "see"))
       end
 
       it "returns a copy with the superset of key/value pairs" do
-        @result.should == Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", "D" => "dee")
+        result.should eql(Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", "D" => "dee"))
       end
     end
 
-    describe "with a duplicate key" do
-      before do
-        @result = @original.put("C", "sea")
-      end
+    context "with a duplicate key" do
+      let(:result) { hash.put("C", "sea") }
 
       it "preserves the original" do
-        @original.should == Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
+        result
+        hash.should eql(Hamster.hash("A" => "aye", "B" => "bee", "C" => "see"))
       end
 
       it "returns a copy with the superset of key/value pairs" do
-        @result.should == Hamster.hash("A" => "aye", "B" => "bee", "C" => "sea")
+        result.should eql(Hamster.hash("A" => "aye", "B" => "bee", "C" => "sea"))
+      end
+    end
+
+    context "with unequal keys which hash to the same value" do
+      let(:hash) { Hamster.hash(DeterministicHash.new('a', 1) => 'aye') }
+
+      it "stores and can retrieve both" do
+        result = hash.put(DeterministicHash.new('b', 1), 'bee')
+        result.get(DeterministicHash.new('a', 1)).should eql('aye')
+        result.get(DeterministicHash.new('b', 1)).should eql('bee')
       end
     end
 
     context "when a String is inserted as key and then mutated" do
-      before do
-        @string = "a string!"
-        @hash = Hamster.hash(@string => 'a value!')
-        @string.upcase!
-      end
-
       it "is not affected" do
-        @hash['a string!'].should == 'a value!'
-        @hash['A STRING!'].should be_nil
+        string = "a string!"
+        hash = Hamster.hash(string => 'a value!')
+        string.upcase!
+        hash['a string!'].should == 'a value!'
+        hash['A STRING!'].should be_nil
       end
     end
   end
