72fd112abd411800c08df5147c3e12299e6ca36b
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:01:49 +0200

    Updated style of Hash#partition spec

--- spec/lib/hamster/hash/partition_spec.rb
+++ spec/lib/hamster/hash/partition_spec.rb
@@ -2,33 +2,29 @@ require "spec_helper"
 require "hamster/hash"
 
 describe Hamster::Hash do
-  before do
-    @hash = Hamster.hash("a" => 1, "b" => 2, "c" => 3, "d" => 4)
-    @part = @hash.partition { |k,v| v % 2 == 0 }
-  end
+  let(:hash) { Hamster.hash("a" => 1, "b" => 2, "c" => 3, "d" => 4) }
+  let(:partition) { hash.partition { |k,v| v % 2 == 0 }}
 
   describe "#partition" do
     it "returns a pair of Hamster::Hashes" do
-      @part.each { |h| h.class.should be(Hamster::Hash) }
+      partition.each { |h| h.class.should be(Hamster::Hash) }
+      partition.should be_frozen
     end
 
     it "returns key/val pairs for which predicate is true in first Hash" do
-      @part[0].should == {"b" => 2, "d" => 4}
+      partition[0].should == {"b" => 2, "d" => 4}
     end
 
     it "returns key/val pairs for which predicate is false in second Hash" do
-      @part[1].should == {"a" => 1, "c" => 3}
+      partition[1].should == {"a" => 1, "c" => 3}
     end
 
     context "from a subclass" do
-      before do
-        @subclass = Class.new(Hamster::Hash)
-        @hash = @subclass.new("a" => 1, "b" => 2, "c" => 3, "d" => 4)
-        @part = @hash.partition { |k,v| v % 2 == 0 }
-      end
-
       it "should return instances of the subclass" do
-        @part.each { |h| h.class.should be(@subclass) }
+        subclass  = Class.new(Hamster::Hash)
+        instance  = subclass.new("a" => 1, "b" => 2, "c" => 3, "d" => 4)
+        partition = instance.partition { |k,v| v % 2 == 0 }
+        partition.each { |h| h.class.should be(subclass) }
       end
     end
   end
