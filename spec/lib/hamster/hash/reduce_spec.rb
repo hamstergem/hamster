3f1e2e57ebe4665f9621224b399baa0b87413e02
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:02:26 +0200

    Updated style of Hash#reduce spec

--- spec/lib/hamster/hash/reduce_spec.rb
+++ spec/lib/hamster/hash/reduce_spec.rb
@@ -4,25 +4,18 @@ require "hamster/hash"
 describe Hamster::Hash do
   [:reduce, :inject, :fold, :foldr].each do |method|
     describe "##{method}" do
-      describe "when empty" do
-        before do
-          @original = Hamster.hash
-          @result = @original.send(method, "ABC") {}
-        end
-
+      context "when empty" do
         it "returns the memo" do
-          @result.should == "ABC"
+          Hamster.hash.send(method, "ABC") {}.should == "ABC"
         end
       end
 
-      describe "when not empty" do
-        before do
-          @original = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
-        end
+      context "when not empty" do
+        let(:hash) { Hamster.hash("A" => "aye", "B" => "bee", "C" => "see") }
 
-        describe "with a block" do
+        context "with a block" do
           before do
-            @result = @original.send(method, 0) { |memo, key, value| memo + 1 }
+            @result = hash.send(method, 0) { |memo, key, value| memo + 1 }
           end
 
           it "returns the final memo" do
@@ -30,17 +23,15 @@ describe Hamster::Hash do
           end
         end
 
-        describe "with no block" do
-          before do
-            @original = Hamster.hash(a: 1, b: 2)
-          end
+        context "with no block" do
+          let(:hash) { Hamster.hash(a: 1, b: 2) }
 
           it "uses a passed string as the name of a method to use instead" do
-            [[:a, 1, :b, 2], [:b, 2, :a, 1]].should include(@original.send(method, "+"))
+            [[:a, 1, :b, 2], [:b, 2, :a, 1]].should include(hash.send(method, "+"))
           end
 
           it "uses a passed symbol as the name of a method to use instead" do
-            [[:a, 1, :b, 2], [:b, 2, :a, 1]].should include(@original.send(method, :+))
+            [[:a, 1, :b, 2], [:b, 2, :a, 1]].should include(hash.send(method, :+))
           end
         end
       end
