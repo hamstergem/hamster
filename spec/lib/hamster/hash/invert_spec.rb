62a8adadbe2d03cb1c33926cc6368465f0cd1fcf
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 22:59:42 +0200

    Updated style of Hash#invert spec, added a test

--- spec/lib/hamster/hash/invert_spec.rb
+++ spec/lib/hamster/hash/invert_spec.rb
@@ -3,12 +3,16 @@ require "hamster/hash"
 
 describe Hamster::Hash do
   describe "#invert" do
-    before do
-      @hash = Hamster.hash(a: 3, b: 2, c: 1)
-    end
+    let(:hash) { Hamster.hash(a: 3, b: 2, c: 1) }
 
     it "uses the existing keys as values and values as keys" do
-      @hash.invert.should eql(Hamster.hash(3 => :a, 2 => :b, 1 => :c))
+      hash.invert.should eql(Hamster.hash(3 => :a, 2 => :b, 1 => :c))
+    end
+
+    it "will select one key/value pair among multiple which have same value" do
+      [Hamster.hash(1 => :a),
+       Hamster.hash(1 => :b),
+       Hamster.hash(1 => :c)].should include(Hamster.hash(a: 1, b: 1, c: 1).invert)
     end
   end
 end
