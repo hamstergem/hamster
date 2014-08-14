50b13c2437a48c13c7510a488af49609ce19e65d
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 14:02:14 +0200

    Updated style of Hash#clear spec, added a case

--- spec/lib/hamster/hash/clear_spec.rb
+++ spec/lib/hamster/hash/clear_spec.rb
@@ -3,26 +3,32 @@ require "hamster/hash"
 
 describe Hamster::Hash do
   describe "#clear" do
-
     [
       [],
       ["A" => "aye"],
       ["A" => "aye", "B" => "bee", "C" => "see"],
     ].each do |values|
-
       describe "on #{values}" do
-        before do
-          @original = Hamster.hash(*values)
-          @result = @original.clear
-        end
+        let(:original) { Hamster.hash(*values) }
+        let(:result)   { original.clear }
 
         it "preserves the original" do
-          @original.should == Hamster.hash(*values)
+          original.should eql(Hamster.hash(*values))
         end
 
         it "returns an empty hash" do
-          @result.should equal(Hamster.hash)
+          result.should equal(Hamster.hash)
+          result.should be_empty
         end
+      end
+    end
+
+    context "on a subclass" do
+      it "returns an empty instance of the subclass" do
+        subclass = Class.new(Hamster::Hash)
+        instance = subclass.new(a: 1, b: 2)
+        instance.clear.class.should be(subclass)
+        instance.clear.should be_empty
       end
     end
   end
