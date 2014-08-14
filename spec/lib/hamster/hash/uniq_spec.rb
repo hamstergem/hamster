751b789f0da7a685ba6e27e083147be1004af4a7
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:04:24 +0200

    Updated style of Hash#uniq spec

--- spec/lib/hamster/hash/uniq_spec.rb
+++ spec/lib/hamster/hash/uniq_spec.rb
@@ -2,14 +2,12 @@ require "spec_helper"
 require "hamster/hash"
 
 describe Hamster::Hash do
-  before do
-    @hash = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see")
-  end
+  let(:hash) { Hamster.hash("A" => "aye", "B" => "bee", "C" => "see") }
 
   [:uniq, :nub, :remove_duplicates].each do |method|
     describe "##{method}" do
       it "returns self" do
-        @hash.send(method).should equal(@hash)
+        hash.send(method).should equal(hash)
       end
     end
   end
