db6fb6f7e2b92d166d97f7b35378e13018ee9f58
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 13:23:56 +0200

    Updating style of Hash#any? spec

--- spec/lib/hamster/hash/any_spec.rb
+++ spec/lib/hamster/hash/any_spec.rb
@@ -4,26 +4,20 @@ require "hamster/hash"
 describe Hamster::Hash do
   [:any?, :exist?, :exists?].each do |method|
     describe "##{method}" do
-      describe "when empty" do
-        before do
-          @hash = Hamster.hash
-        end
-
+      context "when empty" do
         it "with a block returns false" do
-          @hash.send(method) {}.should == false
+          Hamster.hash.send(method) {}.should == false
         end
 
         it "with no block returns false" do
-          @hash.send(method).should == false
+          Hamster.hash.send(method).should == false
         end
       end
 
-      describe "when not empty" do
-        before do
-          @hash = Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", nil => "NIL")
-        end
+      context "when not empty" do
+        let(:hash) { Hamster.hash("A" => "aye", "B" => "bee", "C" => "see", nil => "NIL") }
 
-        describe "with a block" do
+        context "with a block" do
           [
             %w[A aye],
             %w[B bee],
@@ -32,18 +26,18 @@ describe Hamster::Hash do
           ].each do |pair|
 
             it "returns true if the block ever returns true (#{pair.inspect})" do
-              @hash.send(method) { |key, value| key == pair.first && value == pair.last }.should == true
+              hash.send(method) { |key, value| key == pair.first && value == pair.last }.should == true
             end
 
             it "returns false if the block always returns false" do
-              @hash.send(method) { |key, value| key == "D" && value == "dee" }.should == false
+              hash.send(method) { |key, value| key == "D" && value == "dee" }.should == false
             end
           end
         end
 
-        describe "with no block" do
+        context "with no block" do
           it "returns true" do
-            @hash.send(method).should == true
+            hash.send(method).should == true
           end
         end
       end
