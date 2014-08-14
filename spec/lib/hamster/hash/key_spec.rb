d12a7150a19ebea2d75f1d2c02ccc636395e7e1c
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 22:59:54 +0200

    Updated style of Hash#key spec, added a test

--- spec/lib/hamster/hash/key_spec.rb
+++ spec/lib/hamster/hash/key_spec.rb
@@ -3,24 +3,26 @@ require "hamster/hash"
 
 describe Hamster::Hash do
   describe "#key" do
-    before do
-      @hash = Hamster.hash(a: 1, b: 1, c: 2, d: 3)
-    end
+    let(:hash) { Hamster.hash(a: 1, b: 1, c: 2, d: 3) }
 
     it "returns a key associated with the given value, if there is one" do
-      [:a, :b].should include(@hash.key(1))
-      @hash.key(2).should be(:c)
-      @hash.key(3).should be(:d)
+      [:a, :b].should include(hash.key(1))
+      hash.key(2).should be(:c)
+      hash.key(3).should be(:d)
     end
 
     it "returns nil if there is no key associated with the given value" do
-      @hash.key(5).should be_nil
-      @hash.key(0).should be_nil
+      hash.key(5).should be_nil
+      hash.key(0).should be_nil
     end
 
     it "uses #== to compare values for equality" do
-      @hash.key(EqualNotEql.new).should_not be_nil
-      @hash.key(EqlNotEqual.new).should be_nil
+      hash.key(EqualNotEql.new).should_not be_nil
+      hash.key(EqlNotEqual.new).should be_nil
+    end
+
+    it "doesn't use default block if value is not found" do
+      Hamster.hash(a: 1) { fail }.key(2).should be_nil
     end
   end
 end
