170f927c875f03555299ffc96c9a94b9bc2cc4ac
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:00:32 +0200

    Updated style of Hash#merge spec

--- spec/lib/hamster/hash/merge_spec.rb
+++ spec/lib/hamster/hash/merge_spec.rb
@@ -5,50 +5,48 @@ describe Hamster::Hash do
   [:merge, :+].each do |method|
     describe "##{method}" do
       [
-        [[], [], []],
-        [["A" => "aye"], [], ["A" => "aye"]],
-        [["A" => "aye"], ["A" => "bee"], ["A" => "bee"]],
-        [["A" => "aye"], ["B" => "bee"], ["A" => "aye", "B" => "bee"]],
+        [{}, {}, {}],
+        [{"A" => "aye"}, {}, {"A" => "aye"}],
+        [{"A" => "aye"}, {"A" => "bee"}, {"A" => "bee"}],
+        [{"A" => "aye"}, {"B" => "bee"}, {"A" => "aye", "B" => "bee"}],
+        [(1..300).zip(1..300), (150..450).zip(150..450), (1..450).zip(1..450)]
       ].each do |a, b, expected|
-
         describe "for #{a.inspect} and #{b.inspect}" do
-          before do
-            @result = Hamster.hash(*a).send(method, Hamster.hash(*b))
-          end
+          let(:result) { Hamster.hash(a).send(method, Hamster.hash(b)) }
 
           it "returns #{expected.inspect} when passed a Hamster::Hash"  do
-            @result.should == Hamster.hash(*expected)
+            result.should eql(Hamster.hash(expected))
           end
 
           it "returns #{expected.inspect} when passed a Ruby Hash" do
-            Hamster.hash(*a).send(method, Hash[*b]).should == Hamster.hash(*expected)
+            Hamster.hash(a).send(method, Hash[b]).should eql(Hamster.hash(expected))
           end
         end
       end
-    end
 
-    context "when merging with an empty Hash" do
-      it "returns self" do
-        @hash = Hamster.hash(a: 1, b: 2)
-        @hash.merge(Hamster.hash).should be(@hash)
+      context "when merging with an empty Hash" do
+        it "returns self" do
+          hash = Hamster.hash(a: 1, b: 2)
+          hash.send(method, Hamster.hash).should be(hash)
+        end
       end
-    end
 
-    it "sets any duplicate key to the value of block if passed a block" do
-      h1 = Hamster.hash(:a => 2, :b => 1, :d => 5)
-      h2 = Hamster.hash(:a => -2, :b => 4, :c => -3)
-      r = h1.merge(h2) { |k,x,y| nil }
-      r.should eql(Hamster.hash(:a => nil, :b => nil, :c => -3, :d => 5))
+      it "sets any duplicate key to the value of block if passed a block" do
+        h1 = Hamster.hash(:a => 2, :b => 1, :d => 5)
+        h2 = Hamster.hash(:a => -2, :b => 4, :c => -3)
+        r = h1.merge(h2) { |k,x,y| nil }
+        r.should eql(Hamster.hash(:a => nil, :b => nil, :c => -3, :d => 5))
 
-      r = h1.merge(h2) { |k,x,y| "#{k}:#{x+2*y}" }
-      r.should eql(Hamster.hash(:a => "a:-2", :b => "b:9", :c => -3, :d => 5))
+        r = h1.merge(h2) { |k,x,y| "#{k}:#{x+2*y}" }
+        r.should eql(Hamster.hash(:a => "a:-2", :b => "b:9", :c => -3, :d => 5))
 
-      lambda {
-        h1.merge(h2) { |k, x, y| raise(IndexError) }
-      }.should raise_error(IndexError)
+        lambda {
+          h1.merge(h2) { |k, x, y| raise(IndexError) }
+        }.should raise_error(IndexError)
 
-      r = h1.merge(h1) { |k,x,y| :x }
-      r.should eql(Hamster.hash(:a => :x, :b => :x, :d => :x))
+        r = h1.merge(h1) { |k,x,y| :x }
+        r.should eql(Hamster.hash(:a => :x, :b => :x, :d => :x))
+      end
     end
   end
 end
