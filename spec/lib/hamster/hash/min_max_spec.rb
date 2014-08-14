3ee7cd67f41c632aad158198000a203be7ce9f3b
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:00:52 +0200

    Updated style of Hash#min, #max, #min_by, #max_by spec. Added tests

--- spec/lib/hamster/hash/min_max_spec.rb
+++ spec/lib/hamster/hash/min_max_spec.rb
@@ -2,14 +2,12 @@ require "spec_helper"
 require "hamster/hash"
 
 describe Hamster::Hash do
-  before do
-    @hash = Hamster.hash("a" => 3, "b" => 2, "c" => 1)
-  end
+  let(:hash) { Hamster.hash("a" => 3, "b" => 2, "c" => 1) }
 
   [:min, :minimum].each do |method|
     describe "##{method}" do
       it "returns the smallest key/val pair" do
-        @hash.send(method).should == ["a", 3]
+        hash.send(method).should == ["a", 3]
       end
     end
   end
@@ -17,20 +15,36 @@ describe Hamster::Hash do
   [:max, :maximum].each do |method|
     describe "##{method}" do
       it "returns the largest key/val pair" do
-        @hash.send(method).should == ["c", 1]
+        hash.send(method).should == ["c", 1]
       end
     end
   end
 
   describe "#min_by" do
     it "returns the smallest key/val pair (after passing it through a key function)" do
-      @hash.min_by { |k,v| v }.should == ["c", 1]
+      hash.min_by { |k,v| v }.should == ["c", 1]
+    end
+
+    it "returns the first key/val pair yielded by #each in case of a tie" do
+      hash.min_by { 0 }.should == hash.each.first
+    end
+
+    it "returns nil if the hash is empty" do
+      Hamster.hash.min_by { |k,v| v }.should be_nil
     end
   end
 
   describe "#max_by" do
     it "returns the largest key/val pair (after passing it through a key function)" do
-      @hash.max_by { |k,v| v }.should == ["a", 3]
+      hash.max_by { |k,v| v }.should == ["a", 3]
+    end
+
+    it "returns the first key/val pair yielded by #each in case of a tie" do
+      hash.max_by { 0 }.should == hash.each.first
+    end
+
+    it "returns nil if the hash is empty" do
+      Hamster.hash.max_by { |k,v| v }.should be_nil
     end
   end
 end
