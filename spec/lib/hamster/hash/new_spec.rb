a5114ab63db68c98c78cb375e57918e2feef4245
Alex Dowad <alexinbeijing@gmail.com>
2014-08-14 23:01:16 +0200

    Updated style of Hash.new spec, added a test

--- spec/lib/hamster/hash/new_spec.rb
+++ spec/lib/hamster/hash/new_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "hamster/hash"
 
 describe Hamster::Hash do
-  describe "new" do
+  describe ".new" do
     it "is amenable to overriding of #initialize" do
       class SnazzyHash < Hamster::Hash
         def initialize
@@ -15,18 +15,16 @@ describe Hamster::Hash do
     end
 
     describe "from a subclass" do
-      before do
-        @subclass = Class.new(Hamster::Hash)
-        @instance = @subclass.new("some" => "values")
+      it "returns a frozen instance of the subclass" do
+        subclass = Class.new(Hamster::Hash)
+        instance = subclass.new("some" => "values")
+        instance.class.should be(subclass)
+        instance.frozen?.should be true
       end
+    end
 
-      it "returns an instance of the subclass" do
-        @instance.class.should be @subclass
-      end
-
-      it "returns a frozen instance" do
-        @instance.frozen?.should be true
-      end
+    it "accepts an array as initializer" do
+      Hamster::Hash.new([['a', 'b'], ['c', 'd']]).should eql(Hamster.hash('a' => 'b', 'c' => 'd'))
     end
   end
 end
