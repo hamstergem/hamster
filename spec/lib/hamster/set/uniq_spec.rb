6c5f2eabfe1a3b02a3aeefd533316c31a8144d31
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:16:51 +0200

    Updated style of Set#uniq spec

--- spec/lib/hamster/set/uniq_spec.rb
+++ spec/lib/hamster/set/uniq_spec.rb
@@ -2,15 +2,12 @@ require "spec_helper"
 require "hamster/set"
 
 describe Hamster::Set do
-  before do
-    @set = Hamster.set("A", "B", "C")
-  end
+  let(:set) { Hamster.set("A", "B", "C") }
 
   [:uniq, :nub, :remove_duplicates].each do |method|
     describe "##{method}" do
-
       it "returns self" do
-        @set.send(method).should equal(@set)
+        set.send(method).should equal(set)
       end
     end
   end
