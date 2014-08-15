fe9e11279f6ca541f7b92b7ab240e10cf529c030
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:08:02 +0200

    Updated style of Set#difference spec, added a test

--- spec/lib/hamster/set/difference_spec.rb
+++ spec/lib/hamster/set/difference_spec.rb
@@ -10,16 +10,25 @@ describe Hamster::Set do
         [["A"], ["A"], []],
         [%w[A B C], ["B"], %w[A C]],
         [%w[A B C], %w[A C], ["B"]],
+        [%w[A B C D E F G H], [], %w[A B C D E F G H]],
+        [%w[A B C M X Y Z], %w[B C D E F G H I J X], %w[A M Y Z]]
       ].each do |a, b, expected|
-
         describe "for #{a.inspect} and #{b.inspect}" do
-          before do
-            @result = Hamster.set(*a).send(method, Hamster.set(*b))
-          end
+          let(:result) { Hamster.set(*a).send(method, Hamster.set(*b)) }
 
           it "returns #{expected.inspect}"  do
-            @result.should == Hamster.set(*expected)
+            result.should eql(Hamster.set(*expected))
           end
+        end
+      end
+
+      it "works on a wide variety of inputs" do
+        items = ('aa'..'zz').to_a
+        50.times do
+          array1 = items.sample(200)
+          array2 = items.sample(200)
+          result = Hamster::Set.new(array1).send(method, Hamster::Set.new(array2))
+          result.to_a.sort.should eql((array1 - array2).sort)
         end
       end
     end
