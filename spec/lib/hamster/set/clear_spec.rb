024d8ee9f089cba8c37944fa1c5aef4f18cf9f60
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:05:44 +0200

    Updated style of Hash#clear spec

--- spec/lib/hamster/set/clear_spec.rb
+++ spec/lib/hamster/set/clear_spec.rb
@@ -8,29 +8,26 @@ describe Hamster::Set do
       ["A"],
       %w[A B C],
     ].each do |values|
-
       describe "on #{values}" do
-        before do
-          @original = Hamster.set(*values)
-          @result = @original.clear
-        end
+        let(:set) { Hamster.set(*values) }
 
         it "preserves the original" do
-          @original.should == Hamster.set(*values)
+          set.clear
+          set.should eql(Hamster.set(*values))
         end
 
         it "returns an empty set" do
-          @result.should equal(Hamster.set)
+          set.clear.should equal(Hamster.set)
         end
       end
     end
 
     context "from a subclass" do
       it "returns an empty instance of the subclass" do
-        @subclass = Class.new(Hamster::Set)
-        @instance = @subclass.new([:a, :b, :c, :d])
-        @instance.clear.class.should be(@subclass)
-        @instance.clear.should be_empty
+        subclass = Class.new(Hamster::Set)
+        instance = subclass.new([:a, :b, :c, :d])
+        instance.clear.class.should be(subclass)
+        instance.clear.should be_empty
       end
     end
   end
