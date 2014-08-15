18d9e21d6add7fc7fa99620edc6b42f61684acdd
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 12:00:44 +0200

    Updated style of Set#add spec, added a couple tests

--- spec/lib/hamster/set/add_spec.rb
+++ spec/lib/hamster/set/add_spec.rb
@@ -2,37 +2,47 @@ require "spec_helper"
 require "hamster/set"
 
 describe Hamster::Set do
-  before do
-    @original = Hamster.set("A", "B", "C")
-  end
+  let(:original) { Hamster.set("A", "B", "C") }
 
   [:add, :<<].each do |method|
     describe "##{method}" do
       context "with a unique value" do
-        before do
-          @result = @original.send(method, "D")
-        end
+        let(:result) { original.send(method, "D") }
 
         it "preserves the original" do
-          @original.should eql(Hamster.set("A", "B", "C"))
+          result
+          original.should eql(Hamster.set("A", "B", "C"))
         end
 
         it "returns a copy with the superset of values" do
-          @result.should eql(Hamster.set("A", "B", "C", "D"))
+          result.should eql(Hamster.set("A", "B", "C", "D"))
         end
       end
 
       context "with a duplicate value" do
-        before do
-          @result = @original.send(method, "C")
-        end
+        let(:result) { original.send(method, "C") }
 
         it "preserves the original values" do
-          @original.should eql(Hamster.set("A", "B", "C"))
+          result
+          original.should eql(Hamster.set("A", "B", "C"))
         end
 
         it "returns self" do
-          @result.should equal(@original)
+          result.should equal(original)
+        end
+      end
+
+      it "can add nil to a set" do
+        original.add(nil).should eql(Hamster.set("A", "B", "C", nil))
+      end
+
+      it "works on large sets, with many combinations of input" do
+        50.times do
+          array = (1..500).to_a.sample(100)
+          set = Hamster::Set.new(array)
+          to_add = 1000 + rand(1000)
+          set.add(to_add).size.should == 101
+          set.add(to_add).should include(to_add)
         end
       end
     end
