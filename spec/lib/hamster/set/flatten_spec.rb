0d80768d5674fa39a2da41ce833e156533b3f3ca
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:09:50 +0200

    Updated style of Set#flatten spec, added a test

--- spec/lib/hamster/set/flatten_spec.rb
+++ spec/lib/hamster/set/flatten_spec.rb
@@ -9,38 +9,38 @@ describe Hamster do
       [["A", Hamster.set("B"), "C"], %w[A B C]],
       [[Hamster.set("A"), Hamster.set("B"), Hamster.set("C")], %w[A B C]],
     ].each do |values, expected|
-
       describe "on #{values}" do
-        before do
-          @original = Hamster.set(*values)
-          @result = @original.flatten
-        end
+        let(:set) { Hamster.set(*values) }
 
         it "preserves the original" do
-          @original.should == Hamster.set(*values)
+          set.flatten
+          set.should eql(Hamster.set(*values))
         end
 
         it "returns the inlined values" do
-          @result.should == Hamster.set(*expected)
+          set.flatten.should eql(Hamster.set(*expected))
         end
       end
     end
 
-    describe "on an empty set" do
-      before do
-        @result = Hamster.set.flatten
-      end
-
+    context "on an empty set" do
       it "returns an empty set" do
-        @result.should equal(Hamster.set)
+        Hamster.set.flatten.should equal(Hamster.set)
+      end
+    end
+
+    context "on a set with multiple levels of nesting" do
+      it "inlines lower levels of nesting" do
+        set = Hamster.set(Hamster.set(Hamster.set(1)), Hamster.set(Hamster.set(2)))
+        set.flatten.should eql(Hamster.set(1, 2))
       end
     end
 
     context "from a subclass" do
       it "returns an instance of the subclass" do
-        @subclass = Class.new(Hamster::Set)
-        @subclass.new.flatten.class.should be(@subclass)
-        @subclass.new([Hamster.set(1), Hamster.set(2)]).flatten.class.should be(@subclass)
+        subclass = Class.new(Hamster::Set)
+        subclass.new.flatten.class.should be(subclass)
+        subclass.new([Hamster.set(1), Hamster.set(2)]).flatten.class.should be(subclass)
       end
     end
   end
