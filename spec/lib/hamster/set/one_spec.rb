b7bec66dc45ca0b4acff225e7ddf20c538839614
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:13:50 +0200

    Updated style of Set#one? spec, added a test

--- spec/lib/hamster/set/one_spec.rb
+++ spec/lib/hamster/set/one_spec.rb
@@ -3,46 +3,44 @@ require "hamster/set"
 
 describe Hamster::Set do
   describe "#one?" do
-    describe "when empty" do
-      before do
-        @set = Hamster.set
-      end
-
+    context "when empty" do
       it "with a block returns false" do
-        @set.one? {}.should == false
+        Hamster.set.one? {}.should == false
       end
 
       it "with no block returns false" do
-        @set.one?.should == false
+        Hamster.set.one?.should == false
       end
     end
 
-    describe "when not empty" do
-      describe "with a block" do
-        before do
-          @set = Hamster.set("A", "B", "C")
-        end
+    context "when not empty" do
+      context "with a block" do
+        let(:set) { Hamster.set("A", "B", "C") }
 
         it "returns false if the block returns true more than once" do
-          @set.one? { |item| true }.should == false
+          set.one? { |item| true }.should == false
         end
 
         it "returns false if the block never returns true" do
-          @set.one? { |item| false }.should == false
+          set.one? { |item| false }.should == false
         end
 
         it "returns true if the block only returns true once" do
-          @set.one? { |item| item == "A" }.should == true
+          set.one? { |item| item == "A" }.should == true
         end
       end
 
-      describe "with no block" do
+      context "with no block" do
         it "returns false if more than one value is truthy" do
           Hamster.set(nil, true, "A").one?.should == false
         end
 
         it "returns true if only one value is truthy" do
           Hamster.set(nil, true, false).one?.should == true
+        end
+
+        it "returns false if no values are truthy" do
+          Hamster.set(nil, false).one?.should == false
         end
       end
     end
