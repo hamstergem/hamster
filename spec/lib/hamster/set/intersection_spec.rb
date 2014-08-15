2c37f719d93ec50595551594c4ca53510917e513
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:11:41 +0200

    Updated style of Set#intersection spec, added a test

--- spec/lib/hamster/set/intersection_spec.rb
+++ spec/lib/hamster/set/intersection_spec.rb
@@ -11,24 +11,26 @@ describe Hamster::Set do
         [%w[A B C], ["B"], ["B"]],
         [%w[A B C], %w[A C], %w[A C]],
       ].each do |a, b, expected|
-
         describe "returns #{expected.inspect}" do
-          before do
-            @a = Hamster.set(*a)
-            @b = Hamster.set(*b)
-          end
+          let(:set_a) { Hamster.set(*a) }
+          let(:set_b) { Hamster.set(*b) }
 
           it "for #{a.inspect} and #{b.inspect}"  do
-            @result = @a.send(method, @b)
+            set_a.send(method, set_b).should eql(Hamster.set(*expected))
           end
 
           it "for #{b.inspect} and #{a.inspect}"  do
-            @result = @b.send(method, @a)
+            set_b.send(method, set_a).should eql(Hamster.set(*expected))
           end
+        end
+      end
 
-          after  do
-            @result.should == Hamster.set(*expected)
-          end
+      it "returns results consistent with Array#&" do
+        50.times do
+          array1 = rand(100).times.map { rand(1000000).to_s(16) }
+          array2 = rand(100).times.map { rand(1000000).to_s(16) }
+          result = Hamster::Set.new(array1).send(method, Hamster::Set.new(array2))
+          result.to_a.sort.should eql((array1 & array2).sort)
         end
       end
     end
