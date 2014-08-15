cfd017b835b970a32d011a74b060269fcd2cfa5c
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:14:49 +0200

    Updated style of Set#sample spec

--- spec/lib/hamster/set/sample_spec.rb
+++ spec/lib/hamster/set/sample_spec.rb
@@ -3,14 +3,12 @@ require "hamster/set"
 
 describe Hamster::Set do
   describe "#sample" do
-    before do
-      @set = Hamster::Set.new(1..10)
-    end
+    let(:set) { Hamster::Set.new(1..10) }
 
     it "returns a randomly chosen item" do
-      chosen = 100.times.map { @set.sample }
-      chosen.each { |item| @set.should include(item) }
-      @set.each { |item| chosen.should include(item) }
+      chosen = 100.times.map { set.sample }
+      chosen.each { |item| set.should include(item) }
+      set.each { |item| chosen.should include(item) }
     end
   end
 end
