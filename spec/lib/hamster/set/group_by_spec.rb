82d44b885c225b4eb3495187c286cb25fc406acb
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:10:20 +0200

    Updated style of Set#group_by spec, added a couple tests

--- spec/lib/hamster/set/group_by_spec.rb
+++ spec/lib/hamster/set/group_by_spec.rb
@@ -4,21 +4,15 @@ require "hamster/set"
 describe Hamster::Set do
   [:group_by, :group, :classify].each do |method|
     describe "##{method}" do
-      describe "with a block" do
+      context "with a block" do
         [
           [[], []],
           [[1], [true => Hamster.set(1)]],
           [[1, 2, 3, 4], [true => Hamster.set(3, 1), false => Hamster.set(4, 2)]],
         ].each do |values, expected|
-
           describe "on #{values.inspect}" do
-            before do
-              original = Hamster.set(*values)
-              @result = original.send(method, &:odd?)
-            end
-
             it "returns #{expected.inspect}" do
-              @result.should == Hamster.hash(*expected)
+              Hamster.set(*values).send(method, &:odd?).should eql(Hamster.hash(*expected))
             end
           end
         end
@@ -30,25 +24,29 @@ describe Hamster::Set do
           [[1], [1 => Hamster.set(1)]],
           [[1, 2, 3, 4], [1 => Hamster.set(1), 2 => Hamster.set(2), 3 => Hamster.set(3), 4 => Hamster.set(4)]],
         ].each do |values, expected|
-
           describe "on #{values.inspect}" do
-            before do
-              original = Hamster.set(*values)
-              @result = original.group_by
-            end
-
             it "returns #{expected.inspect}" do
-              @result.should == Hamster.hash(*expected)
+              Hamster.set(*values).group_by.should eql(Hamster.hash(*expected))
             end
           end
         end
       end
 
+      context "on an empty set" do
+        it "returns an empty hash" do
+          Hamster.set.group_by { |x| x }.should eql(Hamster.hash)
+        end
+      end
+
+      it "returns a hash without default proc" do
+        Hamster.set(1,2,3).group_by { |x| x }.default_proc.should be_nil
+      end
+
       context "from a subclass" do
         it "returns an Hash whose values are instances of the subclass" do
-          @subclass = Class.new(Hamster::Set)
-          @instance = @subclass.new([1, 'string', :symbol])
-          @instance.group_by { |x| x.class }.values.each { |v| v.class.should be(@subclass) }
+          subclass = Class.new(Hamster::Set)
+          instance = subclass.new([1, 'string', :symbol])
+          instance.group_by { |x| x.class }.values.each { |v| v.class.should be(subclass) }
         end
       end
     end
