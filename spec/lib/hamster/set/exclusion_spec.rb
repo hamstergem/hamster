c1bfc14e3648f672b723c8580f02de69d49fc4ae
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:09:05 +0200

    Updated style of Set#^ spec, added a test case

--- spec/lib/hamster/set/exclusion_spec.rb
+++ spec/lib/hamster/set/exclusion_spec.rb
@@ -12,15 +12,21 @@ describe Hamster::Set do
         [%w[A B C], %w[B C D], %w[A D]],
         [%w[A B C], %w[D E F], %w[A B C D E F]],
       ].each do |a, b, expected|
-
         describe "for #{a.inspect} and #{b.inspect}" do
-          before do
-            @result = Hamster.set(*a).send(method, Hamster.set(*b))
-          end
+          let(:result) { Hamster.set(*a).send(method, Hamster.set(*b)) }
 
           it "returns #{expected.inspect}"  do
-            @result.should == Hamster.set(*expected)
+            result.should eql(Hamster.set(*expected))
           end
+        end
+      end
+
+      it "works for a wide variety of inputs" do
+        50.times do
+          array1 = (1..400).to_a.sample(100)
+          array2 = (1..400).to_a.sample(100)
+          result = Hamster::Set.new(array1) ^ Hamster::Set.new(array2)
+          result.to_a.sort.should eql(((array1 | array2) - (array1 & array2)).sort)
         end
       end
     end
