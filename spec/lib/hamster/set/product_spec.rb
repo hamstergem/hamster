51d4e9cf74412c8c663a660eedb71a8f69157e59
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:14:09 +0200

    Updated style of Set#product spec

--- spec/lib/hamster/set/product_spec.rb
+++ spec/lib/hamster/set/product_spec.rb
@@ -8,15 +8,9 @@ describe Hamster::Set do
       [[2], 2],
       [[1, 3, 5, 7, 11], 1155],
     ].each do |values, expected|
-
       describe "on #{values.inspect}" do
-        before do
-          original = Hamster.set(*values)
-          @result = original.product
-        end
-
         it "returns #{expected.inspect}" do
-          @result.should == expected
+          Hamster.set(*values).product.should == expected
         end
       end
     end
