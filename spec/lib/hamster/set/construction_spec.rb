839917552f3385c759ff96c088587621b02951f1
Alex Dowad <alexinbeijing@gmail.com>
2014-08-15 20:07:12 +0200

    Updated style of Hamster.set spec

--- spec/lib/hamster/set/construction_spec.rb
+++ spec/lib/hamster/set/construction_spec.rb
@@ -3,20 +3,16 @@ require "hamster/set"
 
 describe Hamster::Set do
   describe ".set" do
-    describe "with no values" do
+    context "with no values" do
       it "returns the empty set" do
+        Hamster.set.should be_empty
         Hamster.set.should equal(Hamster::EmptySet)
       end
-
     end
 
-    describe "with a list of values" do
-      before do
-        @set = Hamster.set("A", "B", "C")
-      end
-
+    context "with a list of values" do
       it "is equivalent to repeatedly using #add" do
-        @set.should eql(Hamster.set.add("A").add("B").add("C"))
+        Hamster.set("A", "B", "C").should eql(Hamster.set.add("A").add("B").add("C"))
       end
     end
   end
