75629b72fe3294541d73b1123bb1df11b589a612
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:43:10 +0200

    Updated style of Vector#take spec, added more test cases

--- spec/lib/hamster/vector/take_spec.rb
+++ spec/lib/hamster/vector/take_spec.rb
@@ -8,20 +8,20 @@ describe Hamster::Vector do
       [["A"], 10, ["A"]],
       [%w[A B C], 0, []],
       [%w[A B C], 2, %w[A B]],
+      [(1..32), 1, [1]],
+      [(1..33), 32, (1..32)],
+      [(1..100), 40, (1..40)]
     ].each do |values, number, expected|
-
       describe "#{number} from #{values.inspect}" do
-        before do
-          @original = Hamster.vector(*values)
-          @result = @original.take(number)
-        end
+        let(:vector) { Hamster.vector(*values) }
 
         it "preserves the original" do
-          @original.should eql(Hamster.vector(*values))
+          vector.take(number)
+          vector.should eql(Hamster.vector(*values))
         end
 
         it "returns #{expected.inspect}" do
-          @result.should eql(Hamster.vector(*expected))
+          vector.take(number).should eql(Hamster.vector(*expected))
         end
       end
     end
