c94d8d9239621fe90f583e6436307724d6b2b41c
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:35:13 +0200

    Updated style of Vector#last specs, added a test

--- spec/lib/hamster/vector/last_spec.rb
+++ spec/lib/hamster/vector/last_spec.rb
@@ -13,25 +13,34 @@ describe Hamster::Vector do
       end
     end
 
-    context "with an empty array" do
+    context "with an empty vector" do
       let(:last_item) { nil }
       let(:values) { [] }
 
       include_examples "checking values"
     end
 
-    context "with a single item array" do
+    context "with a single item vector" do
       let(:last_item) { "A" }
       let(:values) { %w[A] }
 
       include_examples "checking values"
     end
 
-    context "with a multi-item array" do
+    context "with a multi-item vector" do
       let(:last_item) { "B" }
       let(:values) { %w[A B] }
 
       include_examples "checking values"
+    end
+
+    [31, 32, 33, 1023, 1024, 1025].each do |size|
+      context "with a #{size}-item vector" do
+        let(:last_item) { size }
+        let(:values) { (1..size).to_a }
+
+        include_examples "checking values"
+      end
     end
   end
 end
