176498576385df99f1239527594819febbd4ca22
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:38:18 +0200

    Updated style of Vector#partition specs

--- spec/lib/hamster/vector/partition_spec.rb
+++ spec/lib/hamster/vector/partition_spec.rb
@@ -13,46 +13,38 @@ describe Hamster::Vector do
       [[3, 4], [3], [4]],
       [[4], [], [4]],
     ].each do |values, expected_matches, expected_remainder|
-
       describe "on #{values.inspect}" do
-        before do
-          @original = Hamster.vector(*values)
-        end
+        let(:vector) { Hamster.vector(*values) }
 
         describe "with a block" do
-          before do
-            @result = @original.partition(&:odd?)
-            @matches = @result.first
-            @remainder = @result.last
-          end
+          let(:result)    { vector.partition(&:odd?) }
+          let(:matches)   { result.first }
+          let(:remainder) { result.last }
 
           it "preserves the original" do
-            @original.should eql(Hamster.vector(*values))
+            result
+            vector.should eql(Hamster.vector(*values))
           end
 
           it "returns a frozen array with two items" do
-            @result.class.should be(Array)
-            @result.should be_frozen
-            @result.size.should be(2)
+            result.class.should be(Array)
+            result.should be_frozen
+            result.size.should be(2)
           end
 
           it "correctly identifies the matches" do
-            @matches.should eql(Hamster.vector(*expected_matches))
+            matches.should eql(Hamster.vector(*expected_matches))
           end
 
           it "correctly identifies the remainder" do
-            @remainder.should eql(Hamster.vector(*expected_remainder))
+            remainder.should eql(Hamster.vector(*expected_remainder))
           end
         end
 
         describe "without a block" do
-          before do
-            @result = @original.partition
-          end
-
           it "returns an Enumerator" do
-            @result.class.should be(Enumerator)
-            @result.each(&:odd?).should eql([Hamster::Vector.new(expected_matches), Hamster::Vector.new(expected_remainder)])
+            vector.partition.class.should be(Enumerator)
+            vector.partition.each(&:odd?).should eql([Hamster::Vector.new(expected_matches), Hamster::Vector.new(expected_remainder)])
           end
         end
       end
