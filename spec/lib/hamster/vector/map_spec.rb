cc0846564870236d3086963b31c79c5a3c320bf8
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:36:17 +0200

    Updated style of Vector#map specs, added a test

--- spec/lib/hamster/vector/map_spec.rb
+++ spec/lib/hamster/vector/map_spec.rb
@@ -4,53 +4,47 @@ require "hamster/vector"
 describe Hamster::Vector do
   [:map, :collect].each do |method|
     describe "##{method}" do
-      describe "when empty" do
-        before do
-          @original = Hamster.vector
-          @mapped = @original.send(method) {}
-        end
+      context "when empty" do
+        let(:vector) { Hamster.vector }
 
         it "returns self" do
-          @mapped.should equal(@original)
+          vector.send(method) {}.should equal(vector)
         end
       end
 
-      describe "when not empty" do
-        before do
-          @original = Hamster.vector("A", "B", "C")
-        end
+      context "when not empty" do
+        let(:vector) { Hamster.vector("A", "B", "C") }
 
-        describe "with a block" do
-          before do
-            @mapped = @original.send(method, &:downcase)
-          end
-
+        context "with a block" do
           it "preserves the original values" do
-            @original.should == Hamster.vector("A", "B", "C")
+            vector.send(method, &:downcase)
+            vector.should eql(Hamster.vector("A", "B", "C"))
           end
 
           it "returns a new vector with the mapped values" do
-            @mapped.should == Hamster.vector("a", "b", "c")
+            vector.send(method, &:downcase).should eql(Hamster.vector("a", "b", "c"))
           end
         end
 
-        describe "with no block" do
-          before do
-            @result = @original.send(method)
-          end
-
+        context "with no block" do
           it "returns an Enumerator" do
-            @result.class.should be(Enumerator)
-            @result.each(&:downcase).should == Hamster.vector('a', 'b', 'c')
+            vector.send(method).class.should be(Enumerator)
+            vector.send(method).each(&:downcase).should eql(Hamster.vector('a', 'b', 'c'))
           end
         end
       end
 
       context "from a subclass" do
         it "returns an instance of the subclass" do
-          @subclass = Class.new(Hamster::Vector)
-          @instance = @subclass[1,2,3]
-          @instance.map { |x| x + 1 }.class.should be(@subclass)
+          subclass = Class.new(Hamster::Vector)
+          instance = subclass[1,2,3]
+          instance.map { |x| x + 1 }.class.should be(subclass)
+        end
+      end
+
+      context "on a large vector" do
+        it "works" do
+          V.new(1..2000).map { |x| x * 2 }.should eql(V.new((1..2000).map { |x| x * 2}))
         end
       end
     end
