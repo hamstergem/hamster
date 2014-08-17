a5b66d700632eeb71ea9151674407428ad658157
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:43:43 +0200

    Updated style of Vector#to_a specs, added more test cases

--- spec/lib/hamster/vector/to_a_spec.rb
+++ spec/lib/hamster/vector/to_a_spec.rb
@@ -13,22 +13,30 @@ describe Hamster::Vector do
       end
     end
 
-    context "with an empty array" do
+    context "with an empty vector" do
       let(:values) { [] }
 
       include_examples "checking to_a values"
     end
 
-    context "with an single item array" do
+    context "with an single item vector" do
       let(:values) { %w[A] }
 
       include_examples "checking to_a values"
     end
 
-    context "with an multi-item array" do
+    context "with an multi-item vector" do
       let(:values) { %w[A B] }
 
       include_examples "checking to_a values"
+    end
+
+    [10, 31, 32, 33, 1000, 1023, 1024, 1025].each do |size|
+      context "with a #{size}-item vector" do
+        let(:values) { (1..size).to_a }
+
+        include_examples "checking to_a values"
+      end
     end
   end
 end
