dc918d48c987863b61eb2808207a636204d56fa3
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 08:39:32 +0200

    Update style of Vector#add spec, added more tests

--- spec/lib/hamster/vector/add_spec.rb
+++ spec/lib/hamster/vector/add_spec.rb
@@ -11,9 +11,9 @@ describe Hamster::Vector do
       let(:added_vector) { Hamster.vector(*added_values) }
 
       it "preserves the original" do
-        orignal = vector
+        original = vector
         vector.add(added_value)
-        expect(orignal).to eq(vector)
+        expect(original).to eq(vector)
       end
 
       it "adds the item to the vector" do
@@ -21,7 +21,7 @@ describe Hamster::Vector do
       end
     end
 
-    context "with a empty array adding a single item" do
+    context "with a empty vector adding a single item" do
       let(:values) { [] }
       let(:added_value) { "A" }
       let(:added_values) { ["A"] }
@@ -29,7 +29,7 @@ describe Hamster::Vector do
       include_examples "checking adding values"
     end
 
-    context "with a single-item array adding a different item" do
+    context "with a single-item vector adding a different item" do
       let(:values) { ["A"] }
       let(:added_value) { "B" }
       let(:added_values) { %w[A B] }
@@ -37,7 +37,7 @@ describe Hamster::Vector do
       include_examples "checking adding values"
     end
 
-    context "with a single-item array adding a duplicate item" do
+    context "with a single-item vector adding a duplicate item" do
       let(:values) { ["A"] }
       let(:added_value) { "A" }
       let(:added_values) { %w[A A] }
@@ -45,11 +45,21 @@ describe Hamster::Vector do
       include_examples "checking adding values"
     end
 
+    [31, 32, 33, 1023, 1024, 1025].each do |size|
+      context "with a #{size}-item vector adding a different item" do
+        let(:values) { (1..size).to_a }
+        let(:added_value) { size+1 }
+        let(:added_values) { (1..(size+1)).to_a }
+
+        include_examples "checking adding values"
+      end
+    end
+
     context "from a subclass" do
       it "returns an instance of the subclass" do
-        @subclass = Class.new(Hamster::Vector)
-        @instance = @subclass[1,2,3]
-        @instance.add(4).class.should be(@subclass)
+        subclass = Class.new(Hamster::Vector)
+        instance = subclass[1,2,3]
+        instance.add(4).class.should be(subclass)
       end
     end
   end
