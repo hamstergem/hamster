deab5c7c37c9a86e0c5969d026e9962fb873b8a4
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:35:36 +0200

    Updated style of Vector#length specs, added a test

--- spec/lib/hamster/vector/length_spec.rb
+++ spec/lib/hamster/vector/length_spec.rb
@@ -13,25 +13,34 @@ describe Hamster::Vector do
       end
     end
 
-    context "with an empty array" do
+    context "with an empty vector" do
       let(:values) { [] }
       let(:size) { 0 }
 
       include_examples "checking size"
     end
 
-    context "with a single item array" do
+    context "with a single item vector" do
       let(:values) { %w[A] }
       let(:size) { 1 }
 
       include_examples "checking size"
     end
 
-    context "with a multi-item array" do
+    context "with a multi-item vector" do
       let(:values) { %w[A B] }
       let(:size) { 2 }
 
       include_examples "checking size"
+    end
+
+    [31, 32, 33, 1023, 1024, 1025].each do |size|
+      context "with a #{size}-item vector" do
+        let(:values) { (1..size).to_a }
+        let(:size) { size }
+
+        include_examples "checking size"
+      end
     end
   end
 end
