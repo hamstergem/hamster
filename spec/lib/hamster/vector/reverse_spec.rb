d491c9b093b44af0e25ce90f02a289a304a8a8f3
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:40:37 +0200

    Added more test cases for Vector#reverse

--- spec/lib/hamster/vector/reverse_spec.rb
+++ spec/lib/hamster/vector/reverse_spec.rb
@@ -7,7 +7,10 @@ describe Hamster::Vector do
       [[], []],
       [[1], [1]],
       [[1,2], [2,1]],
-      [(1..100).to_a, (1..100).to_a.reverse]
+      [(1..32).to_a, (1..32).to_a.reverse],
+      [(1..33).to_a, (1..33).to_a.reverse],
+      [(1..100).to_a, (1..100).to_a.reverse],
+      [(1..1024).to_a, (1..1024).to_a.reverse]
     ].each do |initial, expected|
       describe "on #{initial}" do
         it "returns #{expected}" do
