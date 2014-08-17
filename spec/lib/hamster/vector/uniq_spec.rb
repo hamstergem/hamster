b8d24b938813aa9a46f70f9b46648254b5da9991
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 20:09:53 +0200

    Updated style of Vector#uniq spec, added more test cases

--- spec/lib/hamster/vector/uniq_spec.rb
+++ spec/lib/hamster/vector/uniq_spec.rb
@@ -3,28 +3,38 @@ require "hamster/vector"
 
 describe Hamster::Vector do
   describe "#uniq" do
-    before do
-      @vector = Hamster.vector('a', 'b', 'a', 'a', 'c', 'b')
-    end
+    let(:vector) { Hamster.vector('a', 'b', 'a', 'a', 'c', 'b') }
 
     it "returns a vector with no duplicates" do
-      @vector.uniq.should eql(Hamster.vector('a', 'b', 'c'))
+      vector.uniq.should eql(Hamster.vector('a', 'b', 'c'))
     end
 
     it "leaves the original unmodified" do
-      @vector.uniq
-      @vector.should eql(Hamster.vector('a', 'b', 'a', 'a', 'c', 'b'))
+      vector.uniq
+      vector.should eql(Hamster.vector('a', 'b', 'a', 'a', 'c', 'b'))
     end
 
     it "uses eql? semantics" do
       Hamster.vector(1.0, 1).uniq.should eql(Hamster.vector(1.0, 1))
     end
 
+    [10, 31, 32, 33, 1000, 1023, 1024, 1025, 2000].each do |size|
+      context "on a #{size}-item vector" do
+        it "behaves like Array#uniq" do
+          array = size.times.map { rand(size*2) }
+          vector = V.new(array)
+          result = vector.uniq
+          result.should == array.uniq
+          result.class.should be(Hamster::Vector)
+        end
+      end
+    end
+
     context "from a subclass" do
       it "returns an instance of the subclass" do
-        @subclass = Class.new(Hamster::Vector)
-        @instance = @subclass.new([1,2,3])
-        @instance.uniq.class.should be(@subclass)
+        subclass = Class.new(Hamster::Vector)
+        instance = subclass.new([1,2,3])
+        instance.uniq.class.should be(subclass)
       end
     end
   end
