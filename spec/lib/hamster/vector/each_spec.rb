778eb74f9fea910cead51c2438b02cd75c3cf824
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:30:35 +0200

    Updated style of Vector#each spec, added more test cases

--- spec/lib/hamster/vector/each_spec.rb
+++ spec/lib/hamster/vector/each_spec.rb
@@ -5,30 +5,41 @@ describe Hamster::Vector do
   [:each, :foreach].each do |method|
     describe "##{method}" do
       describe "with no block" do
-        before do
-          @vector = Hamster.vector("A", "B", "C")
-          @result = @vector.send(method)
-        end
+        let(:vector) { Hamster.vector("A", "B", "C") }
 
         it "returns an Enumerator" do
-          @result.class.should be(Enumerator)
-          @result.to_a.should == @vector
+          vector.send(method).class.should be(Enumerator)
+          vector.send(method).to_a.should == vector
         end
       end
 
-      describe "with a block" do
-        before do
-          @vector = Hamster.vector(*(1..1025))
-          @items = []
-          @result = @vector.send(method) { |item| @items << item }
-        end
+      [31, 32, 33, 1023, 1024, 1025].each do |size|
+        context "on a #{size}-item vector" do
+          describe "with a block" do
+            let(:vector) { V.new(1..size) }
 
-        it "returns self" do
-          @result.should be(@vector)
-        end
+            it "returns self" do
+              items = []
+              vector.send(method) { |item| items << item }.should be(vector)
+            end
 
-        it "iterates over the items in order" do
-          @items.should == (1..1025).to_a
+            it "yields all the items" do
+              items = []
+              vector.send(method) { |item| items << item }
+              items.should == (1..size).to_a
+            end
+
+            it "iterates over the items in order" do
+              vector.send(method).first.should == 1
+              vector.send(method).to_a.last.should == size
+            end
+          end
+        end
+      end
+
+      context "on an empty vector" do
+        it "doesn't yield anything" do
+          Hamster.vector.each { fail }
         end
       end
     end
