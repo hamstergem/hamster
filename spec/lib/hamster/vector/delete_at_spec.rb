4e56309ed832ea5997c5b8c2733e70bb3239d213
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:29:20 +0200

    Updated style of Vector#delete_at spec, added more test cases

--- spec/lib/hamster/vector/delete_at_spec.rb
+++ spec/lib/hamster/vector/delete_at_spec.rb
@@ -3,25 +3,38 @@ require "hamster/vector"
 
 describe Hamster::Vector do
   describe "#delete_at" do
-    before do
-      @vector = Hamster.vector(1,2,3,4,5)
-    end
+    let(:vector) { Hamster.vector(1,2,3,4,5) }
 
     it "removes the element at the specified index" do
-      @vector.delete_at(0).should eql(Hamster.vector(2,3,4,5))
-      @vector.delete_at(2).should eql(Hamster.vector(1,2,4,5))
-      @vector.delete_at(-1).should eql(Hamster.vector(1,2,3,4))
+      vector.delete_at(0).should eql(Hamster.vector(2,3,4,5))
+      vector.delete_at(2).should eql(Hamster.vector(1,2,4,5))
+      vector.delete_at(-1).should eql(Hamster.vector(1,2,3,4))
     end
 
     it "makes no modification if the index is out of range" do
-      @vector.delete_at(5).should eql(@vector)
-      @vector.delete_at(-6).should eql(@vector)
+      vector.delete_at(5).should eql(vector)
+      vector.delete_at(-6).should eql(vector)
     end
 
     it "works when deleting last item at boundary where vector trie needs to get shallower" do
       vector = Hamster::Vector.new(1..33)
       vector.delete_at(32).size.should == 32
       vector.delete_at(32).to_a.should eql((1..32).to_a)
+    end
+
+    it "works on an empty vector" do
+      Hamster.vector.delete_at(0).should be(Hamster.vector)
+      Hamster.vector.delete_at(1).should be(Hamster.vector)
+    end
+
+    it "works on a vector with 1 item" do
+      Hamster.vector(10).delete_at(0).should eql(Hamster.vector)
+      Hamster.vector(10).delete_at(1).should eql(Hamster.vector(10))
+    end
+
+    it "works on a vector with 32 items" do
+      V.new(1..32).delete_at(0).should eql(V.new(2..32))
+      V.new(1..32).delete_at(31).should eql(V.new(1..31))
     end
 
     it "has the right size and contents after many deletions" do
