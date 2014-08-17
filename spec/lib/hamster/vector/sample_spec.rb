a626c1dbd334c2bc85d67d019348b766689e7294
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:41:24 +0200

    Updated style of Vector#sample specs

--- spec/lib/hamster/vector/sample_spec.rb
+++ spec/lib/hamster/vector/sample_spec.rb
@@ -3,14 +3,12 @@ require "hamster/vector"
 
 describe Hamster::Vector do
   describe "#sample" do
-    before do
-      @vector = Hamster::Vector.new(1..10)
-    end
+    let(:vector) { Hamster::Vector.new(1..10) }
 
     it "returns a randomly chosen item" do
-      chosen = 100.times.map { @vector.sample }
-      chosen.each { |item| @vector.should include(item) }
-      @vector.each { |item| chosen.should include(item) }
+      chosen = 100.times.map { vector.sample }
+      chosen.each { |item| vector.should include(item) }
+      vector.each { |item| chosen.should include(item) }
     end
   end
 end
