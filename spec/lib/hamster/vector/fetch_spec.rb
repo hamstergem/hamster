6d00476a0b93b7537d4feba0113005646d15e8fd
Alex Dowad <alexinbeijing@gmail.com>
2014-08-17 19:32:34 +0200

    Updated style of Vector#fetch spec

--- spec/lib/hamster/vector/fetch_spec.rb
+++ spec/lib/hamster/vector/fetch_spec.rb
@@ -3,23 +3,21 @@ require "hamster/vector"
 
 describe Hamster::Vector do
   describe "#fetch" do
-    before do
-      @vector = Hamster.vector('a', 'b', 'c')
-    end
+    let(:vector) { Hamster.vector('a', 'b', 'c') }
 
     context "with no default provided" do
       context "when the index exists" do
         it "returns the value at the index" do
-          @vector.fetch(0).should == "a"
-          @vector.fetch(1).should == "b"
-          @vector.fetch(2).should == "c"
+          vector.fetch(0).should == "a"
+          vector.fetch(1).should == "b"
+          vector.fetch(2).should == "c"
         end
       end
 
       context "when the key does not exist" do
         it "raises an IndexError" do
-          -> { @vector.fetch(3) }.should raise_error(IndexError)
-          -> { @vector.fetch(-4) }.should raise_error(IndexError)
+          -> { vector.fetch(3) }.should raise_error(IndexError)
+          -> { vector.fetch(-4) }.should raise_error(IndexError)
         end
       end
     end
@@ -27,16 +25,16 @@ describe Hamster::Vector do
     context "with a default value" do
       context "when the index exists" do
         it "returns the value at the index" do
-          @vector.fetch(0, "default").should == "a"
-          @vector.fetch(1, "default").should == "b"
-          @vector.fetch(2, "default").should == "c"
+          vector.fetch(0, "default").should == "a"
+          vector.fetch(1, "default").should == "b"
+          vector.fetch(2, "default").should == "c"
         end
       end
 
       context "when the index does not exist" do
         it "returns the default value" do
-          @vector.fetch(3, "default").should == "default"
-          @vector.fetch(-4, "default").should == "default"
+          vector.fetch(3, "default").should == "default"
+          vector.fetch(-4, "default").should == "default"
         end
       end
     end
@@ -44,22 +42,22 @@ describe Hamster::Vector do
     context "with a default block" do
       context "when the index exists" do
         it "returns the value at the index" do
-          @vector.fetch(0) { "default".upcase }.should == "a"
-          @vector.fetch(1) { "default".upcase }.should == "b"
-          @vector.fetch(2) { "default".upcase }.should == "c"
+          vector.fetch(0) { "default".upcase }.should == "a"
+          vector.fetch(1) { "default".upcase }.should == "b"
+          vector.fetch(2) { "default".upcase }.should == "c"
         end
       end
 
       context "when the key does not exist" do
         it "returns the default value" do
-          @vector.fetch(3) { "default".upcase }.should == "DEFAULT"
-          @vector.fetch(-4) { "default".upcase }.should == "DEFAULT"
+          vector.fetch(3) { "default".upcase }.should == "DEFAULT"
+          vector.fetch(-4) { "default".upcase }.should == "DEFAULT"
         end
       end
     end
 
     it "gives precedence to default block over default argument if passed both" do
-      @vector.fetch(3, 'one') { 'two' }.should == 'two'
+      vector.fetch(3, 'one') { 'two' }.should == 'two'
     end
   end
 end
