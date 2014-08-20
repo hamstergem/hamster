64e5cc577a586d79f3e22e15f712849b0327bee1
Alex Dowad <alexinbeijing@gmail.com>
2014-08-20 11:43:02 +0200

    Make Hash#sample spec pass more consistently

--- spec/lib/hamster/hash/sample_spec.rb
+++ spec/lib/hamster/hash/sample_spec.rb
@@ -6,7 +6,7 @@ describe Hamster::Hash do
     let(:hash) { Hamster::Hash.new((:a..:z).zip(1..26)) }
 
     it "returns a randomly chosen item" do
-      chosen = 150.times.map { hash.sample }.sort.uniq
+      chosen = 250.times.map { hash.sample }.sort.uniq
       chosen.each { |item| hash.should include(item[0]) }
       hash.each { |item| chosen.should include(item) }
     end
