ce44e1edcb05bd86d84c7345911aec129e291055
Alex Dowad <alexinbeijing@gmail.com>
2014-08-20 11:43:12 +0200

    Added a test for List#add

--- spec/lib/hamster/list/add_spec.rb
+++ spec/lib/hamster/list/add_spec.rb
@@ -5,7 +5,16 @@ describe Hamster::List do
   describe "#add" do
     it "adds an item onto the end of a list" do
       list = Hamster.list("a", "b")
-      list.add("c").should == Hamster.list("a", "b", "c")
+      list.add("c").should eql(Hamster.list("a", "b", "c"))
+      list.should eql(Hamster.list("a", "b"))
+    end
+
+    context "on an empty list" do
+      it "returns a list with one item" do
+        list = Hamster.list
+        list.add("c").should eql(Hamster.list("c"))
+        list.should eql(Hamster.list)
+      end
     end
   end
 end
