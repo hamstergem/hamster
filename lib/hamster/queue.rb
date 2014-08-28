c1b14b920852baa4e1da5d72e54fc6169316fa94
Alex Dowad <alexinbeijing@gmail.com>
2014-08-28 19:48:55 +0200

    Wrote docstrings for all Queue methods

--- lib/hamster/queue.rb
+++ lib/hamster/queue.rb
@@ -42,14 +42,26 @@ module Hamster
     include Immutable
 
     class << self
+      # Create a new `Queue` populated with the given items.
+      # @return [Queue]
       def [](*items)
         items.empty? ? empty : new(items)
       end
 
+      # Return an empty `Queue`. If used on a subclass, returns an empty instance
+      # of that class.
+      #
+      # @return [Queue]
       def empty
         @empty ||= self.new
       end
 
+      # "Raw" allocation of a new `Queue`. Used internally to create a new
+      # instance quickly after consing onto the front/rear lists or taking their
+      # tails.
+      #
+      # @return [Queue]
+      # @private
       def alloc(front, rear)
         result = allocate
         result.instance_variable_set(:@front, front)
@@ -63,16 +75,22 @@ module Hamster
       @rear  = EmptyList
     end
 
+    # Return `true` if this `Queue` contains no items.
+    # @return [Boolean]
     def empty?
       @front.empty? && @rear.empty?
     end
     def_delegator :self, :empty?, :null?
 
+    # Return the number of items in this `Queue`.
+    # @return [Integer]
     def size
       @front.size + @rear.size
     end
     def_delegator :self, :size, :length
 
+    # Return the first item in the `Queue`. If the queue is empty, return `nil`.
+    # @return [Object]
     def first
       return @front.head unless @front.empty?
       @rear.last
@@ -80,12 +98,17 @@ module Hamster
     def_delegator :self, :first, :head
     def_delegator :self, :first, :front
 
+    # Return the last item in the `Queue`. If the queue is empty, return `nil`.
+    # @return [Object]
     def last
       return @rear.head unless @rear.empty?
       @front.last
     end
     def_delegator :self, :last, :peek
 
+    # Return a new `Queue` with `item` added at the end.
+    # @param item [Object] The item to add
+    # @return [Queue]
     def push(item)
       self.class.alloc(@front, @rear.cons(item))
     end
@@ -95,6 +118,8 @@ module Hamster
     def_delegator :self, :push, :conj
     def_delegator :self, :push, :conjoin
 
+    # Return a new `Queue` with the last item removed.
+    # @return [Queue]
     def pop
       front, rear = @front, @rear
 
@@ -106,10 +131,15 @@ module Hamster
       self.class.alloc(front, rear.tail)
     end
 
+    # Return a new `Queue` with `item` added at the front.
+    # @param item [Object] The item to add
+    # @return [Queue]
     def unshift(item)
       self.class.alloc(@front.cons(item), @rear)
     end
 
+    # Return a new `Queue` with the first item removed.
+    # @return [Queue]
     def shift
       front, rear = @front, @rear
 
@@ -123,25 +153,42 @@ module Hamster
     def_delegator :self, :shift, :dequeue
     def_delegator :self, :shift, :tail
 
+    # Return an empty `Queue` instance, of the same class as this one. Useful if you
+    # have multiple subclasses of `Queue` and want to treat them polymorphically.
+    #
+    # @return [Queue]
     def clear
       self.class.empty
     end
 
+    # Return true if `other` has the same type and contents as this `Queue`.
+    #
+    # @param other [Object] The collection to compare with
+    # @return [Boolean]
     def eql?(other)
       instance_of?(other.class) && to_ary.eql?(other.to_ary)
     end
     def_delegator :self, :eql?, :==
 
+    # Return an `Array` with the same elements, in the same order.
+    # @return [Array]
     def to_a
       @front.to_a.concat(@rear.to_a.tap { |a| a.reverse! })
     end
     def_delegator :self, :to_a, :entries
     def_delegator :self, :to_a, :to_ary
 
+    # Return a {List} with the same elements, in the same order.
+    # @return [Hamster::List]
     def to_list
       @front.append(@rear.reverse)
     end
 
+    # Return the contents of this `Queue` as a programmer-readable `String`. If all the
+    # keys and values are serializable as Ruby literal strings, the returned string can
+    # be passed to `eval` to reconstitute an equivalent `Queue`.
+    #
+    # @return [String]
     def inspect
       result = "#{self.class}["
       i = 0
@@ -150,6 +197,7 @@ module Hamster
       result << "]"
     end
 
+    # @private
     def pretty_print(pp)
       pp.group(1, "#{self.class}[", "]") do
         pp.seplist(self.to_a) { |obj| obj.pretty_print(pp) }
