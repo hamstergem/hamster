630d3c505f35a11688fbcc5e2962b86f801a4c55
Alex Dowad <alexinbeijing@gmail.com>
2014-08-29 19:29:20 +0200

    Add Yardoc for ::Enumerable extensions

--- lib/hamster/core_ext/enumerable.rb
+++ lib/hamster/core_ext/enumerable.rb
@@ -1,6 +1,10 @@
 require "hamster/list"
 
+# Ruby's built-in `Enumerable` module.
+# @see http://www.ruby-doc.org/core/Enumerable.html
 module Enumerable
+  # Return a new {Hamster::List} populated with the items in this `Enumerable` object.
+  # @return [List]
   def to_list
     # use destructive operations to build up a new list, like Common Lisp's NCONC
     # this is a very fast way to build up a linked list
