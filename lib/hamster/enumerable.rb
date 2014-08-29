743e37f98ea8e923e1656fb11ddf1ce85e8fcafa
Alex Dowad <alexinbeijing@gmail.com>
2014-08-29 20:40:44 +0200

    Write docstrings for Hamster::Enumerable methods

--- lib/hamster/enumerable.rb
+++ lib/hamster/enumerable.rb
@@ -15,45 +15,64 @@ module Hamster
     extend Forwardable
     include ::Enumerable
 
+    # Return a new collection with all the elements for which the block returns false.
     def remove
       return enum_for(:remove) if not block_given?
       filter { |item| !yield(item) }
     end
 
+    # Return a new collection with all `nil` elements removed.
     def compact
       filter { |item| !item.nil? }
     end
 
+    # Search the collection for elements which are `#===` to `item`. Yield them to
+    # the optional code block if provided, and return them as a new collection.
     def grep(pattern, &block)
       result = filter { |item| pattern === item }
       result = result.map(&block) if block_given?
       result
     end
 
+    # Yield all integers from 0 up to, but not including, the number of items in
+    # this collection. For collections which provide indexed access, these are all
+    # the valid, non-negative indices into the collection.
     def each_index(&block)
       return enum_for(:each_index) unless block_given?
       0.upto(size-1, &block)
       self
     end
 
+    # Multiply all the items (presumably numeric) in this collection together.
     def product
       reduce(1, &:*)
     end
 
+    # Add up all the items (presumably numeric) in this collection.
     def sum
       reduce(0, &:+)
     end
 
+    # Return 2 collections, the first containing all the elements for which the block
+    # evaluates to true, the second containing the rest.
     def partition
       return enum_for(:partition) if not block_given?
       a,b = super
       [self.class.new(a), self.class.new(b)].freeze
     end
 
+    # Combines all elements by applying a binary operation, like `#reduce`, but unlike
+    # `#reduce`, do so from starting from the last element to the first. In other words,
+    # the order in which elements are yielded is the opposite of `#reduce`.
     def foldr(*args, &block)
       reverse.reduce(*args, &block)
     end
 
+    # Groups the collection into sub-collections by the result of yielding them to
+    # the block. Returns a {Hash} where the keys are return values from the block,
+    # and the values are sub-collections. All the sub-collections are built up from
+    # `empty_group`, which should respond to `#conj` by returning a new collection
+    # with an added element.
     def group_by_with(empty_group, &block)
       block ||= lambda { |item| item }
       reduce(EmptyHash) do |hash, item|
@@ -64,10 +83,15 @@ module Hamster
     end
     protected :group_by_with
 
+    # Groups the collection into sub-collections by the result of yielding them to
+    # the block. Returns a {Hash} where the keys are return values from the block,
+    # and the values are sub-collections (of the same type as this one).
     def group_by(&block)
       group_by_with(self.class.empty, &block)
     end
 
+    # Compare with `other`, and return 0, 1, or -1 if it is (respectively) equal to,
+    # greater than, or less than this collection.
     def <=>(other)
       return 0 if self.equal?(other)
       enum1, enum2 = self.to_enum, other.to_enum
@@ -82,10 +106,15 @@ module Hamster
       size1 > size2 ? 1 : -1
     end
 
+    # Return true if `other` contains the same elements, in the same order.
+    # @return [Boolean]
     def ==(other)
       self.eql?(other) || other.respond_to?(:to_ary) && to_ary.eql?(other.to_ary)
     end
 
+    # Convert all the elements into strings and join them together, separated by
+    # `separator`. By default, the `separator` is `$,`, the global default string
+    # separator, which is normally `nil`.
     def join(separator = $,)
       result = ""
       if separator
@@ -96,16 +125,19 @@ module Hamster
       result
     end
 
+    # Convert this collection to a {Set}.
     def to_set
       Set.new(self)
     end
 
+    # Convert this collection to a programmer-readable `String` representation.
     def inspect
       result = "#{self.class}["
       each_with_index { |obj, i| result << ', ' if i > 0; result << obj.inspect }
       result << "]"
     end
 
+    # @private
     def pretty_print(pp)
       pp.group(1, "#{self.class}[", "]") do
         pp.breakable ''
