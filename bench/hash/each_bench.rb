fbd343e7b45eaeef0496d161bcc2f7bec393cf59
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2014-10-22 10:51:54 -0500

    A first attempt at refactoring the benchmark.

--- bench/hash/each_bench.rb
+++ bench/hash/each_bench.rb
@@ -1,37 +1,32 @@
 require "benchmark/ips"
 require "hamster/hash"
 
-Benchmark.ips do |b|
-  sml_hash = Hamster.hash(1 => 1)
-  med_hash = Hamster.hash
-  1_000.times { |i| med_hash = med_hash.put(i, i) }
-  lrg_hash = Hamster.hash
-  1_000_000.times { |i| lrg_hash = lrg_hash.put(i, i) }
+puts ""
+puts "OS Name: #{`uname -a`}"
+puts `sw_vers`
+puts "Ruby Version: #{`ruby -v`}"
+puts "RubyGems Version: #{`gem -v`}"
+puts "RVM Version: #{`rvm -v`}"
+puts ""
 
-  b.report "each small" do |n|
-    a = 0
-    x = 0
-    while a < n
-      sml_hash.each { |y| x = y }
-      a += 1
+SMALL_HASH = { 1 => 2, 2 => 4, 3 => 6, 4 => 8 }
+LARGE_HASH = (1..10_000).inject({}) do |hash, i|
+  hash.merge(i => i * 2)
+end
+
+Benchmark.ips do |analysis|
+  analysis.time = 5
+  analysis.warmup = 2
+
+  analysis.report("Hamster::Hash#each with small data") do
+    Hamster::Hash.new(SMALL_HASH.dup).each do |k, v|
+      k + v
     end
   end
 
-  b.report "each medium" do |n|
-    a = 0
-    x = 0
-    while a < n
-      med_hash.each { |y| x = y }
-      a += 1
-    end
-  end
-
-  b.report "each large" do |n|
-    a = 0
-    x = 0
-    while a < n
-      lrg_hash.each { |y| x = y }
-      a += 1
+  analysis.report("Hamster::Hash#each with large data") do
+    Hamster::Hash.new(LARGE_HASH.dup).each do |k, v|
+      k + v
     end
   end
 end
