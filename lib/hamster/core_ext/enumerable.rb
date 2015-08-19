35c1c37ddbc8d09cdc888a0a57786be925e734e7
Xavier Shay <xavier@rhnh.net>
2015-08-18 18:18:05 -0700

    Remove circular dependency between List and CoreExt

--- lib/hamster/core_ext/enumerable.rb
+++ lib/hamster/core_ext/enumerable.rb
@@ -6,16 +6,6 @@ module Enumerable
   # Return a new {Hamster::List} populated with the items in this `Enumerable` object.
   # @return [List]
   def to_list
-    # use destructive operations to build up a new list, like Common Lisp's NCONC
-    # this is a very fast way to build up a linked list
-    list = tail = Hamster::Cons.allocate
-    each do |item|
-      new_node = Hamster::Cons.allocate
-      new_node.instance_variable_set(:@head, item)
-      tail.instance_variable_set(:@tail, new_node)
-      tail = new_node
-    end
-    tail.instance_variable_set(:@tail, Hamster::EmptyList)
-    list.tail
+    Hamster::List.from_enum(self)
   end
 end
