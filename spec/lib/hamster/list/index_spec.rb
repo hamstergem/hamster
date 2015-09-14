4ece215d92638bbc31ce8d3a3bc5bffd741d154c
Alex Dowad <alexinbeijing@gmail.com>
2015-09-14 22:11:56 +0200

    Skip List#index test on JRuby which is broken by JRuby bug
    I have sent a potential fix to JRuby: https://github.com/jruby/jruby/pull/3325
    
    If the bug is fixed in JRuby, we can later remove this.

--- spec/lib/hamster/list/index_spec.rb
+++ spec/lib/hamster/list/index_spec.rb
@@ -26,7 +26,11 @@ describe Hamster::List do
     ].each do |values, item, expected|
       context "looking for #{item.inspect} in #{values.inspect}" do
         it "returns #{expected.inspect}" do
-          L[*values].index(item).should == expected
+          if RUBY_ENGINE == 'jruby' && RUBY_VERSION <= '2.2.2' && values[0].is_a?(Fixnum) && item.is_a?(Float)
+            skip "On JRuby, Enumerable#find_index doesn't test equality properly"
+          else
+            L[*values].index(item).should == expected
+          end
         end
       end
     end
