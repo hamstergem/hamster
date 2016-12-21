305c0d84dc7643593cbc51c6976e970e20f43e2b
debbbbie <debbbbie@163.com>
2016-12-22 03:10:13 +0800

    Run SimpleCov at local test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 require "pry"
 require "rspec"
