c3b25d8ad2a7902aac8bc10d5fcb79715303a56a
Ivo Anjo <ivo.anjo@ist.utl.pt>
2018-04-21 16:14:04 +0100

    Convert flatten_spec with transpec
    This file needed a manual tweak after transpec conversion, so I decided
    to commit it separately to identify from the fully-automatic conversion
    in the previous commit.

--- spec/lib/hamster/vector/flatten_spec.rb
+++ spec/lib/hamster/vector/flatten_spec.rb
@@ -4,26 +4,26 @@ require "hamster/vector"
 describe Hamster::Vector do
   describe "#flatten" do
     it "recursively flattens nested vectors into containing vector" do
-      V[V[1], V[2]].flatten.should eql(V[1,2])
-      V[V[V[V[V[V[1,2,3]]]]]].flatten.should eql(V[1,2,3])
-      V[V[V[1]], V[V[V[2]]]].flatten.should eql(V[1,2])
+      expect(V[V[1], V[2]].flatten).to eql(V[1,2])
+      expect(V[V[V[V[V[V[1,2,3]]]]]].flatten).to eql(V[1,2,3])
+      expect(V[V[V[1]], V[V[V[2]]]].flatten).to eql(V[1,2])
     end
 
     it "flattens nested arrays as well" do
-      V[[1,2,3],[[4],[5,6]]].flatten.should eql(V[1,2,3,4,5,6])
+      expect(V[[1,2,3],[[4],[5,6]]].flatten).to eql(V[1,2,3,4,5,6])
     end
 
     context "with an integral argument" do
       it "only flattens down to the specified depth" do
-        V[V[V[1,2]]].flatten(1).should eql(V[V[1,2]])
-        V[V[V[V[1]], V[2], V[3]]].flatten(2).should eql(V[V[1], 2, 3])
+        expect(V[V[V[1,2]]].flatten(1)).to eql(V[V[1,2]])
+        expect(V[V[V[V[1]], V[2], V[3]]].flatten(2)).to eql(V[V[1], 2, 3])
       end
     end
 
     context "with an argument of zero" do
       it "returns self" do
         vector = V[1,2,3]
-        vector.flatten(0).should be(vector)
+        expect(vector.flatten(0)).to be(vector)
       end
     end
 
@@ -31,21 +31,21 @@ describe Hamster::Vector do
       it "returns an instance of the subclass" do
         subclass = Class.new(Hamster::Vector)
         instance = subclass.new([1,2])
-        instance.flatten.class.should be(subclass)
+        expect(instance.flatten.class).to be(subclass)
       end
     end
 
     context "on a vector with no nested vectors" do
       it "returns an unchanged vector" do
         vector = V[1,2,3]
-        vector.flatten.should.eql?(V[1,2,3])
+        expect(vector.flatten).to eql(V[1,2,3])
       end
 
       context "on a Vector larger than 32 items initialized with Vector.new" do
         # Regression test, for problem discovered while working on GH issue #182
         it "returns an unchanged vector" do
           vector1,vector2 = 2.times.collect { V.new(0..33) }
-          vector1.flatten.should eql(vector2)
+          expect(vector1.flatten).to eql(vector2)
         end
       end
     end
@@ -53,7 +53,7 @@ describe Hamster::Vector do
     it "leaves the original unmodified" do
       vector = V[1,2,3]
       vector.flatten
-      vector.should eql(V[1,2,3])
+      expect(vector).to eql(V[1,2,3])
     end
   end
 end
